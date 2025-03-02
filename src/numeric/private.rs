812f597a10652d4a6a2a1d39b29b8d7cd93c7048
parrrate <tim@ongoteam.yaconnect.com>
2025-03-02 10:17:39 +0000

    `NonZero`

--- src/numeric/private.rs
+++ src/numeric/private.rs
@@ -1,3 +1,5 @@
+use std::num::NonZero;
+
 use crate::*;
 
 #[derive(ParseAsInline)]
@@ -6,6 +8,14 @@ pub struct Ae<T>(T);
 pub struct Le<T>(T);
 #[derive(ParseAsInline)]
 pub struct Be<T>(T);
+#[derive(ParseAsInline)]
+pub struct Nz<T: NonZeroable>(T::Nz);
+
+pub trait NonZeroable {
+    type Nz: Send + Sync;
+    fn to_nz(&self) -> Option<Self::Nz>;
+    fn from_nz(nz: &Self::Nz) -> Self;
+}
 
 impl<T> From<T> for Ae<T> {
     fn from(n: T) -> Self {
@@ -63,8 +73,26 @@ macro_rules! ae {
             type Le = Ae<$n>;
         }
 
+        impl AsLe for NonZero<$n> {
+            type Le = Nz<Ae<$n>>;
+        }
+
         impl AsBe for $n {
             type Be = Ae<$n>;
+        }
+
+        impl AsBe for NonZero<$n> {
+            type Be = Nz<Ae<$n>>;
+        }
+
+        impl NonZeroable for Ae<$n> {
+            type Nz = NonZero<$n>;
+            fn to_nz(&self) -> Option<Self::Nz> {
+                NonZero::new(self.0)
+            }
+            fn from_nz(nz: &Self::Nz) -> Self {
+                Self(nz.get())
+            }
         }
 
         impl ToOutput for Ae<$n> {
@@ -84,11 +112,16 @@ macro_rules! ae {
             type Size = typenum::generic_const_mappings::U<{ Self::SIZE }>;
         }
 
+        impl MaybeHasNiche for Ae<$n> {
+            type MnArray = NoNiche<Self::Size>;
+        }
+
         impl Topological for Ae<$n> {
             fn accept_points(&self, _: &mut impl PointVisitor) {}
         }
 
         impl Tagged for Ae<$n> {}
+        impl Tagged for Ae<NonZero<$n>> {}
         impl ReflessObject for Ae<$n> {}
         impl ReflessInline for Ae<$n> {}
         impl Object for Ae<$n> {}
@@ -102,8 +135,36 @@ macro_rules! lebe {
             type Le = Le<$n>;
         }
 
+        impl AsLe for NonZero<$n> {
+            type Le = Nz<Le<$n>>;
+        }
+
         impl AsBe for $n {
             type Be = Be<$n>;
+        }
+
+        impl AsBe for NonZero<$n> {
+            type Be = Nz<Be<$n>>;
+        }
+
+        impl NonZeroable for Le<$n> {
+            type Nz = NonZero<$n>;
+            fn to_nz(&self) -> Option<Self::Nz> {
+                NonZero::new(self.0)
+            }
+            fn from_nz(nz: &Self::Nz) -> Self {
+                Self(nz.get())
+            }
+        }
+
+        impl NonZeroable for Be<$n> {
+            type Nz = NonZero<$n>;
+            fn to_nz(&self) -> Option<Self::Nz> {
+                NonZero::new(self.0)
+            }
+            fn from_nz(nz: &Self::Nz) -> Self {
+                Self(nz.get())
+            }
         }
 
         impl ToOutput for Le<$n> {
@@ -138,6 +199,14 @@ macro_rules! lebe {
         impl Size for Be<$n> {
             const SIZE: usize = std::mem::size_of::<$n>();
             type Size = typenum::generic_const_mappings::U<{ Self::SIZE }>;
+        }
+
+        impl MaybeHasNiche for Le<$n> {
+            type MnArray = NoNiche<Self::Size>;
+        }
+
+        impl MaybeHasNiche for Be<$n> {
+            type MnArray = NoNiche<Self::Size>;
         }
 
         impl Topological for Le<$n> {
@@ -175,3 +244,41 @@ lebe!(i64);
 
 lebe!(u128);
 lebe!(i128);
+
+impl<T: NonZeroable + ToOutput> ToOutput for Nz<T> {
+    fn to_output(&self, output: &mut dyn Output) {
+        T::from_nz(&self.0).to_output(output);
+    }
+}
+
+impl<T: NonZeroable + ParseInline<I>, I: ParseInput> ParseInline<I> for Nz<T> {
+    fn parse_inline(input: &mut I) -> crate::Result<Self> {
+        Ok(Self(T::parse_inline(input)?.to_nz().ok_or(Error::Zero)?))
+    }
+}
+
+impl<T: NonZeroable + Size> Size for Nz<T> {
+    type Size = T::Size;
+}
+
+impl<T: NonZeroable + Size> MaybeHasNiche for Nz<T> {
+    type MnArray = SomeNiche<ZeroNiche<T::Size>>;
+}
+
+impl<T: NonZeroable> Topological for Nz<T> {
+    fn accept_points(&self, _: &mut impl PointVisitor) {}
+}
+
+impl<T: NonZeroable> Tagged for Nz<T> {}
+impl<T: NonZeroable + ReflessInline> ReflessObject for Nz<T> {}
+impl<T: NonZeroable + ReflessInline> ReflessInline for Nz<T> {}
+impl<T: NonZeroable + Inline> Object for Nz<T> {}
+impl<T: NonZeroable + Inline> Inline for Nz<T> {}
+
+#[test]
+fn nonzero() {
+    assert_eq!(Option::<Ae<u8>>::SIZE, 2);
+    assert_eq!(Option::<Nz<Ae<u8>>>::SIZE, 1);
+    assert_eq!(Option::<Le<u16>>::SIZE, 3);
+    assert_eq!(Option::<Nz<Le<u16>>>::SIZE, 2);
+}
