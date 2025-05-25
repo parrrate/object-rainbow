454fb21fe05b175e90e5e4f29b769e36f5321abd
parrrate <tim@ongoteam.yaconnect.com>
2025-05-25 15:38:33 +0000

    `PhantomData` `impl`s

---
+++ src/impls/phantom.rs
@@ -0,0 +1,33 @@
+use crate::*;
+
+impl<T: ?Sized> ToOutput for PhantomData<T> {
+    fn to_output(&self, _: &mut dyn Output) {}
+}
+
+impl<T: ?Sized, I: ParseInput> Parse<I> for PhantomData<T> {
+    fn parse(input: I) -> crate::Result<Self> {
+        ParseInline::parse_as_inline(input)
+    }
+}
+
+impl<T: ?Sized, I: ParseInput> ParseInline<I> for PhantomData<T> {
+    fn parse_inline(_: &mut I) -> crate::Result<Self> {
+        Ok(Self)
+    }
+}
+
+impl<T: ?Sized> Topological for PhantomData<T> {}
+impl<T: ?Sized> Tagged for PhantomData<T> {}
+impl<T: ?Sized + 'static + Send + Sync> Object for PhantomData<T> {}
+impl<T: ?Sized + 'static + Send + Sync> Inline for PhantomData<T> {}
+impl<T: ?Sized + 'static + Send + Sync> ReflessObject for PhantomData<T> {}
+impl<T: ?Sized + 'static + Send + Sync> ReflessInline for PhantomData<T> {}
+
+impl<T: ?Sized> Size for PhantomData<T> {
+    const SIZE: usize = 0;
+    type Size = typenum::U0;
+}
+
+impl<T: ?Sized> MaybeHasNiche for PhantomData<T> {
+    type MnArray = NoNiche<ZeroNoNiche<<Self as Size>::Size>>;
+}
