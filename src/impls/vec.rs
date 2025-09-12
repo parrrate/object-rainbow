d05e22e7f531960ac3d113bcaac0a26658dc4f97
parrrate <alisa@parrrate.com>
2025-09-12 18:08:10 +0000

    VecDeque impls

--- src/impls/vec.rs
+++ src/impls/vec.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use crate::*;
 
 impl<T: ToOutput> ToOutput for Vec<T> {
@@ -25,3 +27,29 @@ impl<T: Tagged> Tagged for Vec<T> {
 impl<T: Inline> Object for Vec<T> {}
 
 impl<T: ReflessInline> ReflessObject for Vec<T> {}
+
+impl<T: ToOutput> ToOutput for VecDeque<T> {
+    fn to_output(&self, output: &mut dyn Output) {
+        self.iter_to_output(output);
+    }
+}
+
+impl<T: Topological> Topological for VecDeque<T> {
+    fn accept_points(&self, visitor: &mut impl PointVisitor) {
+        self.iter_accept_points(visitor);
+    }
+}
+
+impl<T: ParseInline<I>, I: ParseInput> Parse<I> for VecDeque<T> {
+    fn parse(input: I) -> crate::Result<Self> {
+        input.parse_collect()
+    }
+}
+
+impl<T: Tagged> Tagged for VecDeque<T> {
+    const TAGS: Tags = T::TAGS;
+}
+
+impl<T: Inline> Object for VecDeque<T> {}
+
+impl<T: ReflessInline> ReflessObject for VecDeque<T> {}
