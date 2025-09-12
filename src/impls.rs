3ba6b848f925bd582bd8602485055f36b719dbbe
parrrate <alisa@parrrate.com>
2025-09-12 18:08:11 +0000

    extract tuple1 impl

--- src/impls.rs
+++ src/impls.rs
@@ -2,4 +2,5 @@ mod btree;
 mod bytes;
 mod refs;
 mod tuple;
+mod tuple1;
 mod vec;
---
+++ src/impls/tuple1.rs
@@ -0,0 +1,41 @@
+use crate::*;
+
+impl<T: ToOutput> ToOutput for (T,) {
+    fn to_output(&self, output: &mut dyn Output) {
+        self.0.to_output(output);
+    }
+}
+
+impl<T: Topological> Topological for (T,) {
+    fn accept_points(&self, visitor: &mut impl PointVisitor) {
+        self.0.accept_points(visitor);
+    }
+}
+
+impl<T: Parse<I>, I: ParseInput> Parse<I> for (T,) {
+    fn parse(input: I) -> crate::Result<Self> {
+        Ok((input.parse()?,))
+    }
+}
+
+impl<T: ParseInline<I>, I: ParseInput> ParseInline<I> for (T,) {
+    fn parse_inline(input: &mut I) -> crate::Result<Self> {
+        Ok((input.parse_inline()?,))
+    }
+}
+
+impl<T: Tagged> Tagged for (T,) {
+    const TAGS: Tags = T::TAGS;
+}
+
+impl<T: Object> Object for (T,) {}
+
+impl<T: Inline> Inline for (T,) {}
+
+impl<T: ReflessObject> ReflessObject for (T,) {}
+
+impl<T: ReflessInline> ReflessInline for (T,) {}
+
+impl<T: Size> Size for (T,) {
+    const SIZE: usize = T::SIZE;
+}
