1f13089f67f9efdac69c494bc8c16ea63c46f54b
parrrate <alisa@parrrate.com>
2025-12-15 03:43:22 +0000

    `&T` `Topological` `Extra`

--- src/impls/refs.rs
+++ src/impls/refs.rs
@@ -6,8 +6,8 @@ impl<T: ToOutput> ToOutput for &T {
     }
 }
 
-impl<T: Topological> Topological for &T {
-    fn accept_points(&self, visitor: &mut impl crate::PointVisitor) {
+impl<T: Topological<E>, E: 'static> Topological<E> for &T {
+    fn accept_points(&self, visitor: &mut impl crate::PointVisitor<E>) {
         (**self).accept_points(visitor);
     }
 }
