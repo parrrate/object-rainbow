aa9be75bfff67d7c112d94d648cc35ff1fbc29cb
parrrate <tim@ongoteam.yaconnect.com>
2025-12-24 13:10:56 +0300

    `Infallible: Size`

--- src/impls/infallible.rs
+++ src/impls/infallible.rs
@@ -1,7 +1,14 @@
+use typenum::U0;
+
 use crate::*;
 
 impl ToOutput for Infallible {
     fn to_output(&self, _: &mut dyn Output) {
         match *self {}
     }
+}
+
+impl Size for Infallible {
+    type Size = U0;
+    const SIZE: usize = 0;
 }
