08f8caac8b3a49bc2dc16c53a640ff42f828a10f
parrrate <tim@ongoteam.yaconnect.com>
2026-01-06 16:27:50 +0300

    `fetchall`

---
+++ object-rainbow-fetchall/examples/test.rs
@@ -0,0 +1,10 @@
+use object_rainbow::Traversible;
+use object_rainbow_fetchall::fetchall;
+
+fn main() -> anyhow::Result<()> {
+    smol::block_on(async move {
+        let map = fetchall(&(().point(), (().point(), ().point()).point()).point()).await?;
+        println!("{}", map.len());
+        Ok(())
+    })
+}
