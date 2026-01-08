01460f3d15feb9ac591384262a22da30344c2ec4
parrrate <tim@ongoteam.yaconnect.com>
2026-01-08 15:56:19 +0300

    `try_fetch_local` for point serialization

--- src/point_serialize.rs
+++ src/point_serialize.rs
@@ -7,10 +7,17 @@ impl<T: Serialize + Clone> Serialize for Point<T> {
     where
         S: serde::Serializer,
     {
-        self.get()
-            .ok_or_else(|| {
-                <S::Error as serde::ser::Error>::custom("cannot serialize remote Point")
-            })?
-            .serialize(serializer)
+        if let Some(object) = self.get() {
+            return object.serialize(serializer);
+        }
+        if let Some((object, _)) = self
+            .try_fetch_local()
+            .map_err(<S::Error as serde::ser::Error>::custom)?
+        {
+            return object.serialize(serializer);
+        }
+        Err(<S::Error as serde::ser::Error>::custom(
+            "cannot serialize remote Point",
+        ))
     }
 }
