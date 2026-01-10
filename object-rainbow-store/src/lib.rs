89fb1a2c27dc17fdcd9dc0e3360e5ec474711b44
parrrate <tim@ongoteam.yaconnect.com>
2026-01-10 16:06:04 +0300

    `RainbowStore::save_point` `impl`

--- object-rainbow-store/src/lib.rs
+++ object-rainbow-store/src/lib.rs
@@ -66,7 +66,7 @@ pub trait RainbowStore: 'static + Send + Sync + Clone {
             Ok(point.with_resolve(self.resolve(), extra))
         }
     }
-    fn save_point<T: Traversible>(&self, point: &Point<T>) -> impl RainbowFuture<T = ()> {
+    fn save_point(&self, point: &Point<impl Traversible>) -> impl RainbowFuture<T = ()> {
         async {
             if !self.contains(point.hash()).await? {
                 self.save_object(&point.fetch().await?).await?;
