64fe49a653a3638f47b16ca5113161f72aae25bb
parrrate <alisa@parrrate.com>
2026-02-19 18:54:52 +0000

    `char: ToOutput`

---
+++ src/impls/char.rs
@@ -0,0 +1,8 @@
+use crate::*;
+
+impl ToOutput for char {
+    fn to_output(&self, output: &mut dyn Output) {
+        let mut buf = [0; 4];
+        self.encode_utf8(&mut buf).to_output(output);
+    }
+}
