fc3f2f367265b0656bf529143afdd9573913b297
parrrate <alisa@parrrate.com>
2026-02-19 19:18:26 +0000

    `char: ParseInline`

--- src/impls/char.rs
+++ src/impls/char.rs
@@ -7,7 +7,59 @@ impl ToOutput for char {
     }
 }
 
+impl<I: ParseInput> Parse<I> for char {
+    fn parse(input: I) -> crate::Result<Self> {
+        Self::parse_as_inline(input)
+    }
+}
+
+impl<I: ParseInput> ParseInline<I> for char {
+    fn parse_inline(input: &mut I) -> crate::Result<Self> {
+        let c0 = input.parse_inline::<u8>()?;
+        if c0 & 0b11000000 != 0b11000000 {
+            let c = str::from_utf8(&[c0])
+                .map_err(crate::Error::parse)?
+                .chars()
+                .next()
+                .unwrap();
+            return Ok(c);
+        }
+        let c1 = input.parse_inline::<u8>()?;
+        if c0 & 0b00100000 == 0 {
+            let c = str::from_utf8(&[c0, c1])
+                .map_err(crate::Error::parse)?
+                .chars()
+                .next()
+                .unwrap();
+            return Ok(c);
+        }
+        let c2 = input.parse_inline::<u8>()?;
+        if c0 & 0b00010000 == 0 {
+            let c = str::from_utf8(&[c0, c1, c2])
+                .map_err(crate::Error::parse)?
+                .chars()
+                .next()
+                .unwrap();
+            return Ok(c);
+        }
+        let c3 = input.parse_inline::<u8>()?;
+        let c = str::from_utf8(&[c0, c1, c2, c3])
+            .map_err(crate::Error::parse)?
+            .chars()
+            .next()
+            .unwrap();
+        Ok(c)
+    }
+}
+
 impl InlineOutput for char {}
 impl Tagged for char {}
 impl ListHashes for char {}
 impl Topological for char {}
+
+#[test]
+fn reparse() -> crate::Result<()> {
+    assert_eq!('x'.reparse()?, 'x');
+    assert_eq!('ч'.reparse()?, 'ч');
+    Ok(())
+}
