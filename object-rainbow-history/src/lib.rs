791d778a99a5e6917e9abfe0b9eeb2e749cd2cdb
parrrate <alisa@parrrate.com>
2026-02-19 16:54:15 +0000

    `History`

---
+++ object-rainbow-history/src/lib.rs
@@ -0,0 +1,53 @@
+use futures_util::TryStreamExt;
+use object_rainbow::{
+    InlineOutput, ListHashes, MaybeHasNiche, Parse, ParseInline, Size, Tagged, ToOutput,
+    Topological, Traversible,
+};
+use object_rainbow_chain_tree::ChainTree;
+
+#[derive(
+    ToOutput, InlineOutput, Tagged, ListHashes, Topological, Parse, ParseInline, Size, MaybeHasNiche,
+)]
+pub struct History<T, D>(ChainTree<(T, D)>);
+
+pub trait Diff<Tree> {
+    fn forward(
+        self,
+        tree: Option<Tree>,
+    ) -> impl Send + Future<Output = object_rainbow::Result<Tree>>;
+}
+
+impl<T: Clone, D: Clone + Diff<T>> History<T, D>
+where
+    (T, D): Traversible,
+{
+    pub async fn commit(&mut self, diff: D) -> object_rainbow::Result<()> {
+        let tree = self.0.prev().await?.last().await?.map(|(tree, _)| tree);
+        let tree = diff.clone().forward(tree).await?;
+        self.0.push((tree, diff)).await?;
+        Ok(())
+    }
+
+    pub async fn forward(&self, other: &Self) -> object_rainbow::Result<()>
+    where
+        T: PartialEq,
+    {
+        other
+            .0
+            .diff(&self.0)
+            .and_then(async |node| {
+                let diff = node.value().1.clone();
+                let tree = node.prev().last().await?.map(|(tree, _)| tree);
+                let tree = diff.clone().forward(tree).await?;
+                if tree == node.value().0 {
+                    Ok(())
+                } else {
+                    Err(object_rainbow::error_fetch!(
+                        "diff doesn't match the new tree",
+                    ))
+                }
+            })
+            .try_collect()
+            .await
+    }
+}
