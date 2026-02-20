4d16e83b1e6803dffc97c422471c9205c4303122
parrrate <alisa@parrrate.com>
2026-02-20 23:42:35 +0000

    `UniqueDiffs`

---
+++ object-rainbow-unique-diffs/src/lib.rs
@@ -0,0 +1,17 @@
+use object_rainbow::FullHash;
+use object_rainbow_amt_set::AmtSet;
+use object_rainbow_history::Forward;
+
+pub struct UniqueDiffs<T> {
+    diffs: AmtSet,
+    tree: T,
+}
+
+impl<T: Forward<D>, D: Send + FullHash> Forward<D> for UniqueDiffs<T> {
+    async fn forward(&mut self, diff: D) -> object_rainbow::Result<()> {
+        if !self.diffs.contains(diff.full_hash()).await? {
+            self.tree.forward(diff).await?;
+        }
+        Ok(())
+    }
+}
