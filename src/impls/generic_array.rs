26a657490029a1bcde8e8acb7a066ca76155d99c
parrrate <alisa@parrrate.com>
2026-02-20 18:16:15 +0000

    `GenericArray: ListHashes`

--- src/impls/generic_array.rs
+++ src/impls/generic_array.rs
@@ -7,3 +7,9 @@ impl<T: InlineOutput, N: ArrayLength> ToOutput for GenericArray<T, N> {
 }
 
 impl<T: InlineOutput, N: ArrayLength> InlineOutput for GenericArray<T, N> {}
+
+impl<T: ListHashes, N: ArrayLength> ListHashes for GenericArray<T, N> {
+    fn list_hashes(&self, f: &mut impl FnMut(Hash)) {
+        self.iter_list_hashes(f);
+    }
+}
