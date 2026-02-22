c1c930866dad7809a16c5de5940996905a81495c
parrrate <alisa@parrrate.com>
2026-02-22 22:26:30 +0000

    `TrieSet: Forward`

--- object-rainbow-history/src/trie.rs
+++ object-rainbow-history/src/trie.rs
@@ -1,5 +1,5 @@
 use object_rainbow::{InlineOutput, ReflessObject, Traversible};
-use object_rainbow_trie::TrieMap;
+use object_rainbow_trie::{TrieMap, TrieSet};
 
 use crate::Forward;
 
@@ -16,6 +16,23 @@ where
                 self.insert(&key, value).await?;
             } else {
                 self.remove(&key).await?;
+            }
+            Ok(())
+        }
+    }
+}
+
+/// `true` represents removal, `false` represents insertion to keep layout equivalence.
+impl<T: ReflessObject> Forward<(T, bool)> for TrieSet<T> {
+    fn forward(
+        &mut self,
+        (value, remove): (T, bool),
+    ) -> impl Send + Future<Output = object_rainbow::Result<()>> {
+        async move {
+            if remove {
+                self.remove(&value).await?;
+            } else {
+                self.insert(&value).await?;
             }
             Ok(())
         }
