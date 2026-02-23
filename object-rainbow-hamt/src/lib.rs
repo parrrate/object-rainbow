70b94036d8234e5c6465fe5a513164e7630f09ad
parrrate <alisa@parrrate.com>
2026-02-23 22:34:03 +0000

    `HamtSet`

--- object-rainbow-hamt/src/lib.rs
+++ object-rainbow-hamt/src/lib.rs
@@ -362,3 +362,38 @@ fn hash_key(hash: Hash) -> K32 {
     ] = hash.into_bytes();
     (x0,(x1,(x2,(x3,(x4,(x5,(x6,(x7,(x8,(x9,(x10,(x11,(x12,(x13,(x14,(x15,(x16,(x17,(x18,(x19,(x20,(x21,(x22,(x23,(x24,(x25,(x26,(x27,(x28,(x29,(x30,(x31))))))))))))))))))))))))))))))))
 }
+
+#[derive(
+    ToOutput,
+    InlineOutput,
+    Tagged,
+    ListHashes,
+    Topological,
+    Parse,
+    ParseInline,
+    Size,
+    MaybeHasNiche,
+    Clone,
+    Default,
+    PartialEq,
+    Eq,
+)]
+pub struct HamtSet(HamtMap<()>);
+
+assert_impl!(
+    impl<E> Inline<E> for HamtSet where E: 'static + Send + Sync + Clone {}
+);
+
+impl HamtSet {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub async fn insert(&mut self, hash: Hash) -> object_rainbow::Result<bool> {
+        Ok(self.0.insert(hash, ()).await?.is_none())
+    }
+
+    pub async fn contains(&self, hash: Hash) -> object_rainbow::Result<bool> {
+        Ok(self.0.get(hash).await?.is_some())
+    }
+}
