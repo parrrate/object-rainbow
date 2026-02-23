91f9ef33d762d520ea93cdd0dff8ec8f6342396d
parrrate <alisa@parrrate.com>
2026-02-23 00:24:34 +0000

    `MapToSet: Sync`

--- object-rainbow-history/src/remap.rs
+++ object-rainbow-history/src/remap.rs
@@ -1,4 +1,4 @@
-pub trait MapToSet<K: Send, V: Send>: Send {
+pub trait MapToSet<K: Send, V: Send>: Send + Sync {
     type T: Send;
     fn map(&self, key: K, value: V)
     -> impl Send + Future<Output = object_rainbow::Result<Self::T>>;
