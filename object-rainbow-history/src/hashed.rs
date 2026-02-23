a9032681cdea52057e6f0d86e3db0e5786aca6e6
parrrate <alisa@parrrate.com>
2026-02-23 22:42:08 +0000

    `HashedDiffs::tree` field

--- object-rainbow-history/src/hashed.rs
+++ object-rainbow-history/src/hashed.rs
@@ -2,7 +2,9 @@ use object_rainbow::{FullHash, Hash};
 
 use crate::Forward;
 
-pub struct HashedDiffs<T>(T);
+pub struct HashedDiffs<T> {
+    tree: T,
+}
 
 impl<T: Forward<Hash>, D: Send + FullHash> Forward<D> for HashedDiffs<T> {
     type Output = T::Output;
@@ -11,6 +13,6 @@ impl<T: Forward<Hash>, D: Send + FullHash> Forward<D> for HashedDiffs<T> {
         &mut self,
         diff: D,
     ) -> impl Send + Future<Output = object_rainbow::Result<Self::Output>> {
-        self.0.forward(diff.full_hash())
+        self.tree.forward(diff.full_hash())
     }
 }
