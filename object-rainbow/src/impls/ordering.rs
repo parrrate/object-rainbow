c7f0d1ec53655ea966285c40cd4e5da64d72f63c
parrrate <alisa@parrrate.com>
2026-03-12 20:00:43 +0000

    `Ordering: InlineOutput`

--- object-rainbow/src/impls/ordering.rs
+++ object-rainbow/src/impls/ordering.rs
@@ -1,9 +1,11 @@
 use std::cmp::Ordering;
 
-use crate::ToOutput;
+use crate::{InlineOutput, ToOutput};
 
 impl ToOutput for Ordering {
     fn to_output(&self, output: &mut dyn crate::Output) {
         (*self as i8).to_output(output);
     }
 }
+
+impl InlineOutput for Ordering {}
