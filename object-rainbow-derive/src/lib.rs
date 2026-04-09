61ed41f3abab27e0ee84f965c1c4852dc1573d4d
parrrate <alisa@parrrate.com>
2026-04-09 11:18:52 +0000

    factor out `derive::contains_generics`

--- object-rainbow-derive/src/lib.rs
+++ object-rainbow-derive/src/lib.rs
@@ -3,74 +3,13 @@ use std::collections::BTreeSet;
 use proc_macro::TokenStream;
 use quote::{quote, quote_spanned};
 use syn::{
-    AngleBracketedGenericArguments, Attribute, Data, DeriveInput, Error, Expr, GenericParam,
-    Generics, Ident, LitStr, Path, Type, parse::Parse, parse_macro_input, parse_quote,
-    parse_quote_spanned, spanned::Spanned, token::Comma,
+    Attribute, Data, DeriveInput, Error, GenericParam, Generics, Ident, LitStr, Type, parse::Parse,
+    parse_macro_input, parse_quote, parse_quote_spanned, spanned::Spanned, token::Comma,
 };
 
-fn expr_contains_generics(g: &BTreeSet<Ident>, expr: &Expr) -> bool {
-    match expr {
-        Expr::Path(expr) => path_contains_generics(g, &expr.path),
-        _ => unimplemented!(),
-    }
-}
+use self::contains_generics::type_contains_generics;
 
-fn args_contains_generics(g: &BTreeSet<Ident>, args: &AngleBracketedGenericArguments) -> bool {
-    args.args.iter().any(|arg| match arg {
-        syn::GenericArgument::Type(ty) => type_contains_generics(g, ty),
-        syn::GenericArgument::AssocType(ty) => {
-            ty.generics
-                .as_ref()
-                .is_some_and(|args| args_contains_generics(g, args))
-                || type_contains_generics(g, &ty.ty)
-        }
-        syn::GenericArgument::Const(expr) => expr_contains_generics(g, expr),
-        syn::GenericArgument::AssocConst(expr) => {
-            expr.generics
-                .as_ref()
-                .is_some_and(|args| args_contains_generics(g, args))
-                || expr_contains_generics(g, &expr.value)
-        }
-        _ => false,
-    })
-}
-
-fn path_contains_generics(g: &BTreeSet<Ident>, path: &Path) -> bool {
-    path.segments.iter().any(|seg| match &seg.arguments {
-        syn::PathArguments::None => !g.is_empty() || g.contains(&seg.ident),
-        syn::PathArguments::AngleBracketed(args) => {
-            seg.ident == "Point" || args_contains_generics(g, args)
-        }
-        syn::PathArguments::Parenthesized(args) => {
-            args.inputs.iter().any(|ty| type_contains_generics(g, ty))
-                || match &args.output {
-                    syn::ReturnType::Default => false,
-                    syn::ReturnType::Type(_, ty) => type_contains_generics(g, ty),
-                }
-        }
-    })
-}
-
-fn type_contains_generics(g: &BTreeSet<Ident>, ty: &Type) -> bool {
-    match ty {
-        Type::Array(ty) => type_contains_generics(g, &ty.elem),
-        Type::BareFn(ty) => {
-            ty.inputs.iter().any(|a| type_contains_generics(g, &a.ty))
-                || match &ty.output {
-                    syn::ReturnType::Default => false,
-                    syn::ReturnType::Type(_, ty) => type_contains_generics(g, ty),
-                }
-        }
-        Type::Group(ty) => type_contains_generics(g, &ty.elem),
-        Type::Macro(_) => true,
-        Type::Paren(ty) => type_contains_generics(g, &ty.elem),
-        Type::Path(ty) => path_contains_generics(g, &ty.path),
-        Type::Reference(ty) => type_contains_generics(g, &ty.elem),
-        Type::Slice(ty) => type_contains_generics(g, &ty.elem),
-        Type::Tuple(ty) => ty.elems.iter().any(|ty| type_contains_generics(g, ty)),
-        _ => false,
-    }
-}
+mod contains_generics;
 
 fn bounds_g(generics: &Generics) -> BTreeSet<Ident> {
     generics
