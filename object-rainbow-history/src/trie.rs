50b6ec3c9368c0f843fed2ac493b67710783dea2
parrrate <alisa@parrrate.com>
2026-04-09 11:20:25 +0000

    trie history test

--- object-rainbow-history/src/trie.rs
+++ object-rainbow-history/src/trie.rs
@@ -55,7 +55,10 @@ mod test {
         let mut history = History::<TrieMap<Vec<u8>, u8>, (Option<u8>, Vec<u8>)>::new();
         history.commit((Some(123), b"abc".into())).await?;
         history = history.reparse()?;
-        assert_eq!(history.tree().await?.get(&b"abc".into()).await?.unwrap(), 123);
+        assert_eq!(
+            history.tree().await?.get(&b"abc".into()).await?.unwrap(),
+            123,
+        );
         Ok(())
     }
 }
