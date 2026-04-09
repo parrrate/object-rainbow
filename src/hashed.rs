17d1d3eaa489de78c0e5f24c3ff9f15ae74ac173
parrrate <alisa@parrrate.com>
2026-04-09 11:18:13 +0000

    `Hashed`

---
+++ src/hashed.rs
@@ -0,0 +1,25 @@
+use std::ops::Deref;
+
+use crate::*;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash, Tagged)]
+pub struct Hashed<T>(T);
+
+impl<T> Deref for Hashed<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T: ToOutput> ToOutput for Hashed<T> {
+    fn to_output(&self, output: &mut dyn object_rainbow::Output) {
+        output.write(&self.0.data_hash());
+    }
+}
+
+impl<T: ToOutput> Size for Hashed<T> {
+    const SIZE: usize = Point::<T>::SIZE;
+    type Size = <Point<T> as Size>::Size;
+}
