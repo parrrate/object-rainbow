3a38ffbb1aabdcf81b4d5907a083178abd692ca4
parrrate <alisa@parrrate.com>
2026-04-09 11:18:42 +0000

    `String` impls

---
+++ src/impls/string.rs
@@ -0,0 +1,18 @@
+use crate::*;
+
+impl ToOutput for String {
+    fn to_output(&self, output: &mut dyn Output) {
+        self.as_str().to_output(output);
+    }
+}
+
+impl<I: ParseInput> Parse<I> for String {
+    fn parse(input: I) -> crate::Result<Self> {
+        Self::from_utf8(input.parse()?).map_err(crate::Error::parse)
+    }
+}
+
+impl Tagged for String {}
+impl<E: 'static> Topological<E> for String {}
+impl<E: 'static> Object<E> for String {}
+impl ReflessObject for String {}
