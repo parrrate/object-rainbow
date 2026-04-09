644834d9000bb4aef7e77e7939215fd6fb20c6bc
parrrate <alisa@parrrate.com>
2026-04-09 11:21:02 +0000

    `inline_extra` (move)

--- object-rainbow/src/lib.rs
+++ object-rainbow/src/lib.rs
@@ -44,6 +44,7 @@ mod hash;
 pub mod hashed;
 mod impls;
 pub mod incr_byte_niche;
+pub mod inline_extra;
 pub mod length_prefixed;
 mod niche;
 pub mod numeric;
@@ -1373,42 +1374,6 @@ pub trait BoundPair: Sized {
 impl<T, E> BoundPair for (T, E) {
     type T = T;
     type E = E;
-}
-
-/// Parses `Extra`, then provides it to `T`'s parser.
-#[derive(Debug, ToOutput, InlineOutput, Tagged, ListHashes, Topological, Clone, Copy)]
-pub struct InlineExtra<T, Extra = ()>(pub Extra, pub T);
-
-impl<
-    E: 'static + Send + Sync + Clone + ParseInline<I>,
-    X: 'static + Send + Sync + Clone,
-    T: Parse<J>,
-    I: PointInput<Extra = X, WithExtra<(E, X)> = J>,
-    J: ParseInput,
-> Parse<I> for InlineExtra<T, E>
-{
-    fn parse(mut input: I) -> crate::Result<Self> {
-        let e = input.parse_inline::<E>()?;
-        let x = input.extra().clone();
-        let t = input.parse_extra((e.clone(), x))?;
-        Ok(Self(e, t))
-    }
-}
-
-impl<
-    E: 'static + Send + Sync + Clone + ParseInline<I>,
-    X: 'static + Send + Sync + Clone,
-    T: ParseInline<J>,
-    I: PointInput<Extra = X, WithExtra<(E, X)> = J>,
-    J: ParseInput,
-> ParseInline<I> for InlineExtra<T, E>
-{
-    fn parse_inline(input: &mut I) -> crate::Result<Self> {
-        let e = input.parse_inline::<E>()?;
-        let x = input.extra().clone();
-        let t = input.parse_inline_extra((e.clone(), x))?;
-        Ok(Self(e, t))
-    }
 }
 
 #[test]
