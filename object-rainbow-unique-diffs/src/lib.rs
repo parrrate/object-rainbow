da34c508da372db0cf308f0ad9f722f7949f433f
parrrate <alisa@parrrate.com>
2026-04-09 11:20:09 +0000

    `UniqueDiffs::tree`

--- object-rainbow-unique-diffs/src/lib.rs
+++ object-rainbow-unique-diffs/src/lib.rs
@@ -7,6 +7,12 @@ pub struct UniqueDiffs<T> {
     tree: T,
 }
 
+impl<T> UniqueDiffs<T> {
+    pub fn tree(&self) -> &T {
+        &self.tree
+    }
+}
+
 impl<T: Forward<D>, D: Send + FullHash> Forward<D> for UniqueDiffs<T> {
     async fn forward(&mut self, diff: D) -> object_rainbow::Result<()> {
         if !self.diffs.contains(diff.full_hash()).await? {
