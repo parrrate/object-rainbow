c671dd2f4144b688084fb81c1ab883b63b4fa7c1
parrrate <alisa@parrrate.com>
2026-04-09 11:19:55 +0000

    `ChainNode: !InlineOutput`

--- object-rainbow-chain-tree/src/lib.rs
+++ object-rainbow-chain-tree/src/lib.rs
@@ -7,7 +7,7 @@ use object_rainbow::{
 use object_rainbow_append_tree::AppendTree;
 use object_rainbow_point::{IntoPoint, Point};
 
-#[derive(ToOutput, InlineOutput, Tagged, ListHashes, Topological, Parse, Clone)]
+#[derive(ToOutput, Tagged, ListHashes, Topological, Parse, Clone)]
 #[topology(recursive)]
 pub struct ChainNode<T> {
     #[tags(skip)]
