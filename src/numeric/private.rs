e9a47068ae51304e115abb41e7edc79b86fb7ea1
parrrate <alisa@parrrate.com>
2026-04-09 11:20:31 +0000

    `Be: Display`

--- src/numeric/private.rs
+++ src/numeric/private.rs
@@ -21,6 +21,12 @@ impl<T: Display> Display for Le<T> {
     }
 }
 
+impl<T: Display> Display for Be<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
 impl<T> Le<T> {
     pub const fn new(value: T) -> Self {
         Self(value)
