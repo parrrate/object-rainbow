73ba153c3b08332a514d5d2050bddbc33d52bcf2
parrrate <alisa@parrrate.com>
2026-04-09 11:18:01 +0000

    `bool: Object`

--- src/impls/bool.rs
+++ src/impls/bool.rs
@@ -32,3 +32,24 @@ impl Niche for BoolNiche {
 impl MaybeHasNiche for bool {
     type MnArray = SomeNiche<BoolNiche>;
 }
+
+impl<I: ParseInput> Parse<I> for bool {
+    fn parse(input: I) -> crate::Result<Self> {
+        ParseInline::parse_as_inline(input)
+    }
+}
+
+impl<I: ParseInput> ParseInline<I> for bool {
+    fn parse_inline(input: &mut I) -> crate::Result<Self> {
+        match input.parse_chunk::<1>()? {
+            [0] => Ok(false),
+            [1] => Ok(true),
+            [_] => Err(Error::OutOfBounds),
+        }
+    }
+}
+
+impl Object for bool {}
+impl Inline for bool {}
+impl ReflessObject for bool {}
+impl ReflessInline for bool {}
