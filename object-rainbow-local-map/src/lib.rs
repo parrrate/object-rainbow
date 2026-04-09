2f0f8507a0e672509c46fb46b813eca9676168b3
parrrate <alisa@parrrate.com>
2026-04-09 11:19:05 +0000

    `LocalMap`

---
+++ object-rainbow-local-map/src/lib.rs
@@ -0,0 +1,40 @@
+use imbl::HashMap;
+use object_rainbow::{Hash, ObjectHashes, ToOutput};
+
+#[derive(Clone)]
+struct Entry {
+    topology: Vec<Hash>,
+    data: Vec<u8>,
+}
+
+pub struct LocalMap {
+    map: HashMap<Hash, Entry>,
+}
+
+impl LocalMap {
+    pub fn insert(
+        &mut self,
+        hash: Hash,
+        tags_hash: Hash,
+        topology: Vec<Hash>,
+        data: Vec<u8>,
+    ) -> object_rainbow::Result<()> {
+        let expected = ObjectHashes {
+            tags: tags_hash,
+            topology: topology.data_hash(),
+            data: data.data_hash(),
+        }
+        .data_hash();
+        if expected != hash {
+            return Err(object_rainbow::Error::DataMismatch);
+        }
+        self.map.insert(hash, Entry { topology, data });
+        Ok(())
+    }
+
+    pub fn get(&self, hash: Hash) -> Option<(&[Hash], &[u8])> {
+        self.map
+            .get(&hash)
+            .map(|Entry { topology, data }| (&**topology, &**data))
+    }
+}
