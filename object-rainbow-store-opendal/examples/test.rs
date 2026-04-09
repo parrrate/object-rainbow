3018442c84cf5580ae2618af45ea7ac920b28f8a
parrrate <alisa@parrrate.com>
2026-04-09 11:18:33 +0000

    opendal example/test

---
+++ object-rainbow-store-opendal/examples/test.rs
@@ -0,0 +1,27 @@
+use object_rainbow::{Fetch, FullHash, Point};
+use object_rainbow_store::RainbowStore;
+use object_rainbow_store_opendal::OpendalStore;
+use opendal::{Operator, services::Memory};
+
+fn main() -> anyhow::Result<()> {
+    smol::block_on(async move {
+        let store = OpendalStore::from_operator(Operator::new(Memory::default())?.finish());
+        let mut point = store
+            .save_point(&Point::from_object((
+                Point::from_object((*b"alisa", *b"feistel")),
+                Point::from_object([1, 2, 3, 4]),
+            )))
+            .await?;
+        assert_eq!(point.fetch().await?.0.fetch().await?.0, *b"alisa");
+        assert_eq!(point.fetch().await?.0.fetch().await?.1, *b"feistel");
+        assert_eq!(point.fetch().await?.1.fetch().await?, [1, 2, 3, 4]);
+        println!("{}", hex::encode(point.full_hash()));
+        point.fetch_mut().await?.1.fetch_mut().await?[3] = 5;
+        assert_eq!(point.fetch().await?.1.fetch().await?, [1, 2, 3, 5]);
+        println!("{}", hex::encode(point.full_hash()));
+        point.fetch_mut().await?.1.fetch_mut().await?[3] = 4;
+        assert_eq!(point.fetch().await?.1.fetch().await?, [1, 2, 3, 4]);
+        println!("{}", hex::encode(point.full_hash()));
+        Ok(())
+    })
+}
