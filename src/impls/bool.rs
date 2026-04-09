537dc6ea89053af712db75d6504e3638bd941d19
parrrate <alisa@parrrate.com>
2026-04-09 11:18:46 +0000

    deeper `bool` niche

--- src/impls/bool.rs
+++ src/impls/bool.rs
@@ -1,5 +1,7 @@
+use std::ops::Add;
+
 use generic_array::GenericArray;
-use typenum::{ToInt, U1, U2};
+use typenum::{B0, B1, IsGreater, IsLess, ToInt, U1, U2, U255, U256};
 
 use crate::*;
 
@@ -15,13 +17,41 @@ impl Size for bool {
 
 pub struct BoolNiche<K>(K);
 
-impl<K: ToInt<u8>> Niche for BoolNiche<K> {
+pub trait NextNiche {
+    type NextNiche;
+}
+
+pub trait WrapNext {
+    type Wrap<J>;
+}
+
+impl WrapNext for B1 {
+    type Wrap<J> = SomeNiche<BoolNiche<J>>;
+}
+
+impl WrapNext for B0 {
+    type Wrap<J> = NoNiche<ZeroNoNiche<U1>>;
+}
+
+impl<
+    K: IsGreater<U1, Output = B1>
+        + IsLess<U256, Output = B1>
+        + Add<B1, Output = J>
+        + IsLess<U255, Output = B>,
+    J,
+    B: WrapNext,
+> NextNiche for K
+{
+    type NextNiche = B::Wrap<J>;
+}
+
+impl<K: ToInt<u8> + NextNiche> Niche for BoolNiche<K> {
     type NeedsTag = typenum::B0;
     type N = U1;
     fn niche() -> GenericArray<u8, Self::N> {
         GenericArray::from_array([K::INT])
     }
-    type Next = NoNiche<ZeroNoNiche<Self::N>>;
+    type Next = K::NextNiche;
 }
 
 impl MaybeHasNiche for bool {
