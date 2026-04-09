616b34e7b37a237ae87e8fbe585785a19ff3ef9f
parrrate <alisa@parrrate.com>
2026-04-09 11:18:28 +0000

    `WithKey`

--- object-rainbow-encrypted/src/lib.rs
+++ object-rainbow-encrypted/src/lib.rs
@@ -2,34 +2,45 @@ use std::{convert::Infallible, ops::Deref, sync::Arc};
 
 use object_rainbow::{
     Address, ByteNode, Error, FailFuture, Fetch, Hash, Input, Object, Parse, ParseInput,
-    ParseSlice, ParseSliceExtra, Point, PointInput, PointVisitor, RawPoint, Resolve, Singular,
-    Tagged, ToOutput, ToOutputExt, Topological, length_prefixed::Lp,
+    ParseSliceExtra, Point, PointInput, PointVisitor, RawPoint, Resolve, Singular, Tagged,
+    ToOutput, ToOutputExt, Topological, length_prefixed::Lp,
 };
 use sha2::{Digest, Sha256};
+
+#[derive(Clone)]
+pub struct WithKey<K, Extra> {
+    pub key: K,
+    pub extra: Extra,
+}
 
 pub trait Key: 'static + Sized + Send + Sync + Clone {
     fn encrypt(&self, data: &[u8]) -> Vec<u8>;
     fn decrypt(&self, data: &[u8]) -> object_rainbow::Result<Vec<u8>>;
 }
 
-type Resolution<K> = Arc<Lp<Vec<RawPoint<Encrypted<K, Infallible>, K>>>>;
+type Resolution<K, Extra> =
+    Arc<Lp<Vec<RawPoint<Encrypted<K, Infallible, Extra>, WithKey<K, Extra>>>>>;
 
 #[derive(ToOutput, Clone)]
 struct Unkeyed<T>(T);
 
-impl<'a, T: Parse<Input<'a>>, K> Parse<Input<'a, K>> for Unkeyed<T> {
-    fn parse(input: Input<'a, K>) -> object_rainbow::Result<Self> {
-        Ok(Self(T::parse(input.replace_extra(&()))?))
+impl<'a, T: Parse<Input<'a, Extra>>, K: 'static + Clone, Extra: 'static + Clone>
+    Parse<Input<'a, WithKey<K, Extra>>> for Unkeyed<T>
+{
+    fn parse(input: Input<'a, WithKey<K, Extra>>) -> object_rainbow::Result<Self> {
+        Ok(Self(T::parse(
+            input.map_extra(|WithKey { extra, .. }| extra),
+        )?))
     }
 }
 
 #[derive(ToOutput, Parse)]
-struct EncryptedInner<K, T> {
-    resolution: Resolution<K>,
+struct EncryptedInner<K, T, Extra> {
+    resolution: Resolution<K, Extra>,
     decrypted: Unkeyed<Arc<T>>,
 }
 
-impl<K, T> Clone for EncryptedInner<K, T> {
+impl<K, T, Extra> Clone for EncryptedInner<K, T, Extra> {
     fn clone(&self) -> Self {
         Self {
             resolution: self.resolution.clone(),
@@ -38,26 +49,33 @@ impl<K, T> Clone for EncryptedInner<K, T> {
     }
 }
 
-struct IterateResolution<'a, K, V> {
-    resolution: std::slice::Iter<'a, RawPoint<Encrypted<K, Infallible>, K>>,
+type ResolutionIter<'a, K, Extra> =
+    std::slice::Iter<'a, RawPoint<Encrypted<K, Infallible, Extra>, WithKey<K, Extra>>>;
+
+struct IterateResolution<'a, K, V, Extra> {
+    resolution: ResolutionIter<'a, K, Extra>,
     visitor: &'a mut V,
 }
 
-impl<'a, K: Key, V: PointVisitor<K>> PointVisitor for IterateResolution<'a, K, V> {
-    fn visit<T: Object>(&mut self, _: &Point<T>) {
+impl<'a, K: Key, V: PointVisitor<WithKey<K, Extra>>, Extra: 'static + Send + Sync + Clone>
+    PointVisitor<Extra> for IterateResolution<'a, K, V, Extra>
+{
+    fn visit<T: Object<Extra>>(&mut self, _: &Point<T, Extra>) {
         let point = self
             .resolution
             .next()
             .expect("length mismatch")
             .clone()
-            .cast::<Encrypted<K, T>>()
+            .cast::<Encrypted<K, T, Extra>>()
             .point();
         self.visitor.visit(&point);
     }
 }
 
-impl<K: Key, T: Topological> Topological<K> for EncryptedInner<K, T> {
-    fn accept_points(&self, visitor: &mut impl PointVisitor<K>) {
+impl<K: Key, T: Topological<Extra>, Extra: 'static + Send + Sync + Clone>
+    Topological<WithKey<K, Extra>> for EncryptedInner<K, T, Extra>
+{
+    fn accept_points(&self, visitor: &mut impl PointVisitor<WithKey<K, Extra>>) {
         self.decrypted.0.accept_points(&mut IterateResolution {
             resolution: self.resolution.iter(),
             visitor,
@@ -73,18 +91,18 @@ impl<K: Key, T: Topological> Topological<K> for EncryptedInner<K, T> {
     }
 }
 
-pub struct Encrypted<K, T> {
+pub struct Encrypted<K, T, Extra> {
     key: K,
-    inner: EncryptedInner<K, T>,
+    inner: EncryptedInner<K, T, Extra>,
 }
 
-impl<K, T: Clone> Encrypted<K, T> {
+impl<K, T: Clone, Extra> Encrypted<K, T, Extra> {
     pub fn into_inner(self) -> T {
         Arc::unwrap_or_clone(self.inner.decrypted.0)
     }
 }
 
-impl<K, T> Deref for Encrypted<K, T> {
+impl<K, T, Extra> Deref for Encrypted<K, T, Extra> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -92,7 +110,7 @@ impl<K, T> Deref for Encrypted<K, T> {
     }
 }
 
-impl<K: Clone, T> Clone for Encrypted<K, T> {
+impl<K: Clone, T, Extra> Clone for Encrypted<K, T, Extra> {
     fn clone(&self) -> Self {
         Self {
             key: self.key.clone(),
@@ -101,8 +119,10 @@ impl<K: Clone, T> Clone for Encrypted<K, T> {
     }
 }
 
-impl<K: Key, T: Topological> Topological<K> for Encrypted<K, T> {
-    fn accept_points(&self, visitor: &mut impl PointVisitor<K>) {
+impl<K: Key, T: Topological<Extra>, Extra: 'static + Send + Sync + Clone>
+    Topological<WithKey<K, Extra>> for Encrypted<K, T, Extra>
+{
+    fn accept_points(&self, visitor: &mut impl PointVisitor<WithKey<K, Extra>>) {
         self.inner.accept_points(visitor);
     }
 
@@ -111,7 +131,7 @@ impl<K: Key, T: Topological> Topological<K> for Encrypted<K, T> {
     }
 }
 
-impl<K: Key, T: ToOutput> ToOutput for Encrypted<K, T> {
+impl<K: Key, T: ToOutput, Extra> ToOutput for Encrypted<K, T, Extra> {
     fn to_output(&self, output: &mut dyn object_rainbow::Output) {
         let source = self.inner.output::<Vec<u8>>();
         output.write(&self.key.encrypt(&source));
@@ -119,11 +139,11 @@ impl<K: Key, T: ToOutput> ToOutput for Encrypted<K, T> {
 }
 
 #[derive(Clone)]
-struct Decrypt<K> {
-    resolution: Resolution<K>,
+struct Decrypt<K, Extra> {
+    resolution: Resolution<K, Extra>,
 }
 
-impl<K: Key> Resolve for Decrypt<K> {
+impl<K: Key, Extra: 'static + Send + Sync + Clone> Resolve for Decrypt<K, Extra> {
     fn resolve(&'_ self, address: Address) -> FailFuture<'_, ByteNode> {
         Box::pin(async move {
             let Encrypted {
@@ -138,7 +158,7 @@ impl<K: Key> Resolve for Decrypt<K> {
                 .get(address.index)
                 .ok_or(Error::AddressOutOfBounds)?
                 .clone()
-                .cast::<Encrypted<K, Vec<u8>>>()
+                .cast::<Encrypted<K, Vec<u8>, Extra>>()
                 .fetch()
                 .await?;
             Ok((
@@ -153,41 +173,63 @@ impl<K: Key> Resolve for Decrypt<K> {
     }
 }
 
-impl<K: Key, T: for<'a> Parse<Input<'a>>> Parse<Input<'_, K>> for Encrypted<K, T> {
-    fn parse(input: Input<'_, K>) -> object_rainbow::Result<Self> {
-        let key = input.extra().clone();
+impl<K: Key, T: for<'a> Parse<Input<'a, Extra>>, Extra: 'static + Send + Sync + Clone>
+    Parse<Input<'_, WithKey<K, Extra>>> for Encrypted<K, T, Extra>
+{
+    fn parse(input: Input<'_, WithKey<K, Extra>>) -> object_rainbow::Result<Self> {
+        let with_key = input.extra().clone();
         let resolve = input.resolve().clone();
-        let source = key.decrypt(input.parse_all())?;
+        let source = with_key.key.decrypt(input.parse_all())?;
         let EncryptedInner {
             resolution,
             decrypted,
-        } = EncryptedInner::<K, Vec<u8>>::parse_slice_extra(&source, &resolve, &key)?;
-        let decrypted = T::parse_slice(
+        } = EncryptedInner::<K, Vec<u8>, Extra>::parse_slice_extra(&source, &resolve, &with_key)?;
+        let decrypted = T::parse_slice_extra(
             &decrypted.0,
             &(Arc::new(Decrypt {
                 resolution: resolution.clone(),
             }) as _),
+            &with_key.extra,
         )?;
         let decrypted = Unkeyed(Arc::new(decrypted));
         let inner = EncryptedInner {
             resolution,
             decrypted,
         };
-        Ok(Self { key, inner })
+        Ok(Self {
+            key: with_key.key,
+            inner,
+        })
     }
 }
 
-impl<K, T> Tagged for Encrypted<K, T> {}
+impl<K, T, Extra> Tagged for Encrypted<K, T, Extra> {}
 
-impl<K: Key, T: Object> Object<K> for Encrypted<K, T> {}
+impl<K: Key, T: Object<Extra>, Extra: 'static + Send + Sync + Clone> Object<WithKey<K, Extra>>
+    for Encrypted<K, T, Extra>
+{
+}
 
-struct ExtractResolution<'a, K>(
-    &'a mut Vec<FailFuture<'static, RawPoint<Encrypted<K, Infallible>, K>>>,
-    &'a K,
-);
+type Extracted<K, Extra> = Vec<
+    std::pin::Pin<
+        Box<
+            dyn Future<
+                    Output = Result<
+                        RawPoint<Encrypted<K, Infallible, Extra>, WithKey<K, Extra>>,
+                        Error,
+                    >,
+                > + Send
+                + 'static,
+        >,
+    >,
+>;
 
-impl<K: Key> PointVisitor for ExtractResolution<'_, K> {
-    fn visit<T: Object>(&mut self, point: &Point<T>) {
+struct ExtractResolution<'a, K, Extra>(&'a mut Extracted<K, Extra>, &'a K);
+
+impl<K: Key, Extra: 'static + Send + Sync + Clone> PointVisitor<Extra>
+    for ExtractResolution<'_, K, Extra>
+{
+    fn visit<T: Object<Extra>>(&mut self, point: &Point<T, Extra>) {
         let point = point.clone();
         let key = self.1.clone();
         self.0.push(Box::pin(async move {
@@ -197,11 +239,11 @@ impl<K: Key> PointVisitor for ExtractResolution<'_, K> {
     }
 }
 
-pub async fn encrypt_point<K: Key, T: Object>(
+pub async fn encrypt_point<K: Key, T: Object<Extra>, Extra: 'static + Send + Sync + Clone>(
     key: K,
-    point: Point<T>,
-) -> object_rainbow::Result<Point<Encrypted<K, T>, K>> {
-    if let Some((address, decrypt)) = point.extract_resolve::<Decrypt<K>>() {
+    point: Point<T, Extra>,
+) -> object_rainbow::Result<Point<Encrypted<K, T, Extra>, WithKey<K, Extra>>> {
+    if let Some((address, decrypt)) = point.extract_resolve::<Decrypt<K, Extra>>() {
         let point = decrypt
             .resolution
             .get(address.index)
@@ -210,14 +252,20 @@ pub async fn encrypt_point<K: Key, T: Object>(
     }
     let decrypted = point.fetch().await?;
     let encrypted = encrypt(key.clone(), decrypted).await?;
-    let point = Point::from_object_extra(encrypted, key);
+    let point = Point::from_object_extra(
+        encrypted,
+        WithKey {
+            key,
+            extra: point.extra().clone(),
+        },
+    );
     Ok(point)
 }
 
-pub async fn encrypt<K: Key, T: Object>(
+pub async fn encrypt<K: Key, T: Object<Extra>, Extra: 'static + Send + Sync + Clone>(
     key: K,
     decrypted: T,
-) -> object_rainbow::Result<Encrypted<K, T>> {
+) -> object_rainbow::Result<Encrypted<K, T, Extra>> {
     let mut futures = Vec::new();
     decrypted.accept_points(&mut ExtractResolution(&mut futures, &key));
     let resolution = futures_util::future::try_join_all(futures).await?;
