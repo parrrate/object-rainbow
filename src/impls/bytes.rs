d10f3cf3f20ab2630455cff72511bb85bd507cf8
parrrate <alisa@parrrate.com>
2026-04-09 11:20:02 +0000

    generalise array parsing

--- src/impls/bytes.rs
+++ src/impls/bytes.rs
@@ -2,18 +2,6 @@ use typenum::ToUInt;
 
 use crate::*;
 
-impl<const N: usize, I: ParseInput> Parse<I> for [u8; N] {
-    fn parse(input: I) -> crate::Result<Self> {
-        ParseInline::parse_as_inline(input)
-    }
-}
-
-impl<const N: usize, I: ParseInput> ParseInline<I> for [u8; N] {
-    fn parse_inline(input: &mut I) -> crate::Result<Self> {
-        input.parse_array()
-    }
-}
-
 impl<const N: usize> MaybeHasNiche for [u8; N]
 where
     typenum::generic_const_mappings::Const<N>: ToUInt<Output: ArrayLength>,
