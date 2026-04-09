7e9597f2aa11405453268285e1fd9e7591bcab1a
parrrate <alisa@parrrate.com>
2026-04-09 11:20:09 +0000

    `UniqueDiffs: Inline`

--- object-rainbow-unique-diffs/src/lib.rs
+++ object-rainbow-unique-diffs/src/lib.rs
@@ -1,11 +1,24 @@
-use object_rainbow::FullHash;
+use object_rainbow::{
+    FullHash, Inline, InlineOutput, ListHashes, Parse, ParseInline, Tagged, ToOutput, Topological,
+    assert_impl,
+};
 use object_rainbow_amt_set::AmtSet;
 use object_rainbow_history::Forward;
 
+#[derive(ToOutput, InlineOutput, Tagged, ListHashes, Topological, Parse, ParseInline)]
 pub struct UniqueDiffs<T> {
     diffs: AmtSet,
     tree: T,
 }
+
+assert_impl!(
+    impl<T, E> Inline<E> for UniqueDiffs<T>
+    where
+        E: 'static + Send + Sync + Clone,
+        T: Inline<E>,
+    {
+    }
+);
 
 impl<T> UniqueDiffs<T> {
     pub fn tree(&self) -> &T {
