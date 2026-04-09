646129ceecc8907804462213f8c1353c8f8b5a64
parrrate <alisa@parrrate.com>
2026-04-09 11:18:59 +0000

    `CString: ParseInline`

--- src/impls/cstring.rs
+++ src/impls/cstring.rs
@@ -10,3 +10,21 @@ impl ToOutput for CString {
 
 impl Topological for CString {}
 impl Tagged for CString {}
+
+impl<I: ParseInput> Parse<I> for CString {
+    fn parse(input: I) -> crate::Result<Self> {
+        Self::parse_as_inline(input)
+    }
+}
+
+impl<I: ParseInput> ParseInline<I> for CString {
+    fn parse_inline(input: &mut I) -> crate::Result<Self> {
+        let mut vec = Vec::new();
+        while let x = input.parse_inline()?
+            && x != 0
+        {
+            vec.push(x);
+        }
+        Ok(Self::new(vec).expect("should stop on zero"))
+    }
+}
