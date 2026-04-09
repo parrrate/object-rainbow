46f2de19cb9e5e45860dbc887dacb4367ecf932c
parrrate <alisa@parrrate.com>
2026-04-09 11:20:58 +0000

    `CStr` mangling

--- object-rainbow/src/impls/cstr.rs
+++ object-rainbow/src/impls/cstr.rs
@@ -4,7 +4,9 @@ use crate::*;
 
 impl ToOutput for CStr {
     fn to_output(&self, output: &mut dyn Output) {
-        self.to_bytes_with_nul().to_output(output);
+        if output.is_real() {
+            self.to_bytes_with_nul().to_output(output);
+        }
     }
 }
 
