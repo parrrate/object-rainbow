0f946040265985f12addbf1e5a7c78a8812e25f4
parrrate <alisa@parrrate.com>
2026-04-09 11:19:00 +0000

    `mod error;`

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ pub use typenum;
 use typenum::Unsigned;
 
 pub use self::enumkind::Enum;
+pub use self::error::{Error, Result};
 pub use self::hash::{Hash, OptionalHash};
 pub use self::niche::{
     AutoEnumNiche, AutoNiche, HackNiche, MaybeHasNiche, Niche, NicheForUnsized, NoNiche, OneNiche,
@@ -33,6 +34,7 @@ pub use self::niche::{
 pub use self::niche::{MaybeNiche, MnArray, NicheFoldOrArray, NicheOr};
 
 pub mod enumkind;
+mod error;
 mod hash;
 pub mod hashed;
 mod impls;
@@ -44,84 +46,6 @@ mod point_deserialize;
 #[cfg(feature = "point-serialize")]
 mod point_serialize;
 mod sha2_const;
-
-#[macro_export]
-/// Construct [`Error::Parse`].
-macro_rules! error_parse {
-    ($($t:tt)*) => {
-        $crate::Error::Parse($crate::anyhow!($($t)*))
-    };
-}
-
-#[macro_export]
-/// Construct [`Error::Fetch`].
-macro_rules! error_fetch {
-    ($($t:tt)*) => {
-        $crate::Error::Fetch($crate::anyhow!($($t)*))
-    };
-}
-
-/// Errors encountered during fetching an object. Mostly related to parsing.
-#[derive(Debug, thiserror::Error)]
-#[non_exhaustive]
-pub enum Error {
-    /// Arbitrary parsing error.
-    #[error(transparent)]
-    Parse(anyhow::Error),
-    /// Arbitrary fetching error.
-    #[error(transparent)]
-    Fetch(anyhow::Error),
-    /// Data left after an [`Inline`] got parsed.
-    #[error("extra input left")]
-    ExtraInputLeft,
-    /// EOF.
-    #[error("end of input")]
-    EndOfInput,
-    /// Overran [`PointInput`]'s [`Address`] vector.
-    #[error("address index out of bounds")]
-    AddressOutOfBounds,
-    /// [`Address::hash`] doesn't match what [`Resolve`] returned.
-    #[error("hash resolution mismatch")]
-    ResolutionMismatch,
-    /// [`FullHash::full_hash`] doesn't match [`Singular::hash`].
-    #[error("data hash mismatch")]
-    DataMismatch,
-    /// Discriminant out of range for an [`Enum`].
-    #[error("discriminant overflow")]
-    DiscriminantOverflow,
-    /// Unepxected zero for a non-zero value.
-    #[error("zero")]
-    Zero,
-    /// Value out of bounds for a certain type.
-    #[error("out of bounds")]
-    OutOfBounds,
-    /// Current architecture (32-bit) is unable to handle lengths of this size.
-    #[error("length out of bounds")]
-    UnsupportedLength,
-    /// Not UTF-8.
-    #[error(transparent)]
-    Utf8(std::string::FromUtf8Error),
-    /// [`Resolve::extension`] (or related things) were unable to resolve the extension.
-    #[error("unknown extension")]
-    UnknownExtension,
-    /// Extension type didn't match what we asked for. This might be turned into panic later.
-    #[error("wrong extension type")]
-    ExtensionType,
-}
-
-impl Error {
-    /// Construct [`Error::Parse`] from another error.
-    pub fn parse(e: impl Into<anyhow::Error>) -> Self {
-        Self::Parse(e.into())
-    }
-
-    /// Construct [`Error::Fetch`] from another error.
-    pub fn fetch(e: impl Into<anyhow::Error>) -> Self {
-        Self::Fetch(e.into())
-    }
-}
-
-pub type Result<T> = std::result::Result<T, Error>;
 
 /// SHA-256 hash size in bytes.
 pub const HASH_SIZE: usize = sha2_const::Sha256::DIGEST_SIZE;
