2ae1cc3490070591b021e5c6127f3f754b233044
parrrate <alisa@parrrate.com>
2026-04-09 11:20:47 +0000

    `Ordering: ParseInline`

--- object-rainbow/src/impls/ordering.rs
+++ object-rainbow/src/impls/ordering.rs
@@ -12,3 +12,20 @@ impl InlineOutput for Ordering {}
 impl Tagged for Ordering {}
 impl ListHashes for Ordering {}
 impl Topological for Ordering {}
+
+impl<I: ParseInput> Parse<I> for Ordering {
+    fn parse(input: I) -> crate::Result<Self> {
+        Self::parse_as_inline(input)
+    }
+}
+
+impl<I: ParseInput> ParseInline<I> for Ordering {
+    fn parse_inline(input: &mut I) -> crate::Result<Self> {
+        match input.parse_inline::<i8>()? {
+            -1 => Ok(Self::Less),
+            0 => Ok(Self::Equal),
+            1 => Ok(Self::Greater),
+            _ => Err(Error::OutOfBounds),
+        }
+    }
+}
