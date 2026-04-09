66e2ab1df726aeb624ba2f95d6b9d1f9f188219b
parrrate <alisa@parrrate.com>
2026-04-09 11:20:57 +0000

    `char` mangling

--- object-rainbow/src/impls/char.rs
+++ object-rainbow/src/impls/char.rs
@@ -2,8 +2,10 @@ use crate::*;
 
 impl ToOutput for char {
     fn to_output(&self, output: &mut dyn Output) {
-        let mut buf = [0; 4];
-        self.encode_utf8(&mut buf).to_output(output);
+        if output.is_real() {
+            let mut buf = [0; 4];
+            self.encode_utf8(&mut buf).to_output(output);
+        }
     }
 }
 
