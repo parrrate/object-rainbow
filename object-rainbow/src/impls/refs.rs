d18b2a52311fb96b736917f350d74644e46e8f27
parrrate <alisa@parrrate.com>
2026-04-09 11:20:56 +0000

    `?Sized` `&T`

--- object-rainbow/src/impls/refs.rs
+++ object-rainbow/src/impls/refs.rs
@@ -1,14 +1,14 @@
 use crate::*;
 
-impl<T: ToOutput> ToOutput for &T {
+impl<T: ?Sized + ToOutput> ToOutput for &T {
     fn to_output(&self, output: &mut dyn Output) {
         (**self).to_output(output);
     }
 }
 
-impl<T: InlineOutput> InlineOutput for &T {}
+impl<T: ?Sized +InlineOutput> InlineOutput for &T {}
 
-impl<T: ListHashes> ListHashes for &T {
+impl<T: ?Sized +ListHashes> ListHashes for &T {
     fn list_hashes(&self, f: &mut impl FnMut(Hash)) {
         (**self).list_hashes(f);
     }
@@ -22,12 +22,12 @@ impl<T: ListHashes> ListHashes for &T {
     }
 }
 
-impl<T: Tagged> Tagged for &T {
+impl<T: ?Sized +Tagged> Tagged for &T {
     const TAGS: Tags = T::TAGS;
     const HASH: Hash = T::HASH;
 }
 
-impl<T: Topological> Topological for &T {
+impl<T: ?Sized +Topological> Topological for &T {
     fn traverse(&self, visitor: &mut impl PointVisitor) {
         (**self).traverse(visitor);
     }
