99e51b85d7d2f901fc14ff321bcf8bfcd6d5e753
parrrate <alisa@parrrate.com>
2026-04-09 11:21:11 +0000

    `InnerFetch::fetch_bytes_local`

--- object-rainbow-encrypted/src/lib.rs
+++ object-rainbow-encrypted/src/lib.rs
@@ -252,6 +252,10 @@ impl<K, D> FetchBytes for InnerFetch<K, D> {
     fn fetch_data(&'_ self) -> FailFuture<'_, Vec<u8>> {
         self.encrypted.fetch_data()
     }
+
+    fn fetch_bytes_local(&self) -> object_rainbow::Result<Option<ByteNode>> {
+        self.encrypted.fetch_bytes_local()
+    }
 }
 
 impl<K: Key, D: Fetch<T: Topological + Tagged>> Fetch for InnerFetch<K, D> {
