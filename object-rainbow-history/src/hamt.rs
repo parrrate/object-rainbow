3e7d93a003839096809e89d38f31f309cd742c19
parrrate <alisa@parrrate.com>
2026-04-17 23:29:40 +0000

    `HamtMap::remove` `Apply`

--- object-rainbow-history/src/hamt.rs
+++ object-rainbow-history/src/hamt.rs
@@ -3,6 +3,25 @@ use object_rainbow_hamt::{HamtMap, HamtSet};
 
 use crate::Apply;
 
+impl<V: 'static + Send + Sync + Clone + Traversible + InlineOutput> Apply<(Option<V>, Hash)>
+    for HamtMap<V>
+{
+    type Output = Option<V>;
+
+    fn apply(
+        &mut self,
+        (value, hash): (Option<V>, Hash),
+    ) -> impl Send + Future<Output = object_rainbow::Result<Self::Output>> {
+        async move {
+            if let Some(value) = value {
+                self.insert(hash, value).await
+            } else {
+                self.remove(hash).await
+            }
+        }
+    }
+}
+
 impl<V: 'static + Send + Sync + Clone + Traversible + InlineOutput> Apply<(V, Hash)>
     for HamtMap<V>
 {
