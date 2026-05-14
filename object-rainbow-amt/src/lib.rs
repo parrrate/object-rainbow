3b218a3675875dc629e7eb5b36f9fc05be70094a
parrrate <alisa@parrrate.com>
2026-05-14 18:46:47 +0000

    `AmtMap: PartialEq`

--- object-rainbow-amt/src/lib.rs
+++ object-rainbow-amt/src/lib.rs
@@ -405,7 +405,16 @@ fn common_length(a: &[u8], b: &[u8]) -> object_rainbow::Result<usize> {
 }
 
 #[derive(
-    Debug, Clone, ToOutput, InlineOutput, Tagged, ListHashes, Topological, Parse, ParseInline,
+    Debug,
+    Clone,
+    ToOutput,
+    InlineOutput,
+    Tagged,
+    ListHashes,
+    Topological,
+    Parse,
+    ParseInline,
+    PartialEq,
 )]
 pub struct AmtMap<K, V>(Node<K, V>);
 
