b5c9ead1279230aefc52c302fa4d6ab6a64c3e76
parrrate <alisa@parrrate.com>
2026-05-14 19:00:31 +0000

    `bool: Collision<((), T)>`

--- object-rainbow-history/src/enforce_unique.rs
+++ object-rainbow-history/src/enforce_unique.rs
@@ -140,6 +140,26 @@ impl<T: Send> Collision<T, ConcealedState> for bool {
     }
 }
 
+impl<T: Send> Collision<((), T), ExposedState> for bool {
+    type Output = ();
+
+    fn always_okay(((), _): &((), T)) -> bool {
+        false
+    }
+
+    fn okay(self) -> Self::Output {
+        assert!(self);
+    }
+
+    fn check(self) -> object_rainbow::Result<Self::Output> {
+        if self {
+            Ok(())
+        } else {
+            Err(object_rainbow::Error::consistency(NotUnique))
+        }
+    }
+}
+
 impl<T: Send> Collision<(bool, T), ExposedState> for bool {
     type Output = bool;
 
