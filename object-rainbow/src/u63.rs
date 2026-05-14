11dd53125988751f88d34f5bc82d0dd3517401fc
parrrate <alisa@parrrate.com>
2026-05-14 16:59:25 +0000

    `U63: ParseInline`

--- object-rainbow/src/u63.rs
+++ object-rainbow/src/u63.rs
@@ -1,6 +1,6 @@
 use crate::*;
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, ParseAsInline)]
 pub struct U63(u64);
 
 impl U63 {
@@ -22,7 +22,7 @@ impl ToOutput for U63 {
         if self.0 < 128 {
             (self.0 as u8).to_output(output);
         } else {
-            let mut bytes = self.0.to_be_bytes();
+            let mut bytes = (self.0 - 128).to_be_bytes();
             bytes[0] ^= 128;
             bytes.to_output(output);
         }
@@ -33,3 +33,17 @@ impl InlineOutput for U63 {}
 impl Tagged for U63 {}
 impl ListHashes for U63 {}
 impl Topological for U63 {}
+
+impl<I: ParseInput> ParseInline<I> for U63 {
+    fn parse_inline(input: &mut I) -> crate::Result<Self> {
+        let mut bytes = [0; 8];
+        input.read(&mut bytes[..1])?;
+        Ok(Self(if bytes[0] & 128 == 0 {
+            bytes[0] as u64
+        } else {
+            bytes[0] ^= 128;
+            input.read(&mut bytes[1..])?;
+            u64::from_be_bytes(bytes) + 128
+        }))
+    }
+}
