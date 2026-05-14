4c7fa594440ea5f8d8b975b7685d8f778db63304
parrrate <alisa@parrrate.com>
2026-05-14 18:41:40 +0000

    `AmtMap: Clone`

--- object-rainbow-amt/src/lib.rs
+++ object-rainbow-amt/src/lib.rs
@@ -394,6 +394,7 @@ fn common_length(a: &[u8], b: &[u8]) -> object_rainbow::Result<usize> {
     }
 }
 
+#[derive(Clone)]
 pub struct AmtMap<K, V>(Node<K, V>);
 
 impl<K, V> Default for AmtMap<K, V> {
