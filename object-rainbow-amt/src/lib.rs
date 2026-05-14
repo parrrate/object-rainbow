ebe3f4340068dba86068261a372f4c21e99f6c98
parrrate <alisa@parrrate.com>
2026-05-14 18:48:12 +0000

    `AmtSet: Topological`

--- object-rainbow-amt/src/lib.rs
+++ object-rainbow-amt/src/lib.rs
@@ -455,7 +455,7 @@ impl<K: InlineOutput + Traversible + Clone, V: InlineOutput + Traversible + Clon
     }
 }
 
-#[derive(Debug, Clone, ToOutput, InlineOutput, Tagged, ListHashes)]
+#[derive(Debug, Clone, ToOutput, InlineOutput, Tagged, ListHashes, Topological)]
 pub struct AmtSet<T>(AmtMap<T, ()>);
 
 impl<T> Default for AmtSet<T> {
