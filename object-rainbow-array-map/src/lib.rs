923a6d596a0e45eefac8202e84027ef827a48274
parrrate <alisa@parrrate.com>
2026-05-15 18:29:16 +0000

    `OccupiedEntry::get_mut`

--- object-rainbow-array-map/src/lib.rs
+++ object-rainbow-array-map/src/lib.rs
@@ -319,6 +319,10 @@ impl<'a, T> OccupiedEntry<'a, T> {
         self.map.get(self.key).unwrap()
     }
 
+    pub fn get_mut(&mut self) -> &mut T {
+        self.map.get_mut(self.key).unwrap()
+    }
+
     pub fn into_mut(self) -> &'a mut T {
         self.map.get_mut(self.key).unwrap()
     }
