2ed20e722f6858b60bcd1bdf1eedb4f00649dd6c
parrrate <alisa@parrrate.com>
2026-05-15 19:45:29 +0000

    `BytesMut: ToOutput`

--- object-rainbow/src/impls/bytes.rs
+++ object-rainbow/src/impls/bytes.rs
@@ -1,8 +1,14 @@
-use bytes::Bytes;
+use bytes::{Bytes, BytesMut};
 
 use crate::*;
 
 impl ToOutput for Bytes {
+    fn to_output(&self, output: &mut impl Output) {
+        (**self).to_output(output);
+    }
+}
+
+impl ToOutput for BytesMut {
     fn to_output(&self, output: &mut impl Output) {
         (**self).to_output(output);
     }
