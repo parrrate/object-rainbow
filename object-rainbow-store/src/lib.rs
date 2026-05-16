4ab4245787a6303f87592114b214b0f69331f4fe
parrrate <alisa@parrrate.com>
2026-05-16 19:35:54 +0000

    `ExternalStore: Send`

--- object-rainbow-store/src/lib.rs
+++ object-rainbow-store/src/lib.rs
@@ -407,7 +407,7 @@ impl<S: RainbowStore, T: Traversible> Stored<S, T> {
     }
 }
 
-pub trait ExternalStore: 'static {
+pub trait ExternalStore: 'static + Send {
     type Id: ReflessInline + Clone + Eq;
     fn save_data(&self, data: &[u8]) -> impl RainbowFuture<T = Self::Id>;
     fn contains_data(&self, data: &[u8]) -> impl RainbowFuture<T = bool>;
