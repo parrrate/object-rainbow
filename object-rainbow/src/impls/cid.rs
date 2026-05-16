08e0920340e1107f51c23a61f6fb281bddcbbe8b
parrrate <alisa@parrrate.com>
2026-05-16 18:04:44 +0000

    `Error: From<cid::Error>`

--- object-rainbow/src/impls/cid.rs
+++ object-rainbow/src/impls/cid.rs
@@ -2,6 +2,15 @@ use cid::CidGeneric;
 
 use crate::*;
 
+impl From<cid::Error> for crate::Error {
+    fn from(error: cid::Error) -> Self {
+        match error {
+            cid::Error::Io(e) => e.into(),
+            e => crate::Error::parse(e),
+        }
+    }
+}
+
 impl<const S: usize> ToOutput for CidGeneric<S> {
     fn to_output(&self, output: &mut impl Output) {
         self.write_bytes(output.as_write())
