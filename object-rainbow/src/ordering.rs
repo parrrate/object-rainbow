3436beb1ee67d16dbe3318b44cccb3f4d5789550
parrrate <alisa@parrrate.com>
2026-05-19 22:48:44 +0000

    `ByteOrdered: PartialOrd`

--- object-rainbow/src/ordering.rs
+++ object-rainbow/src/ordering.rs
@@ -3,7 +3,7 @@ use std::cmp::Ordering;
 use crate::*;
 
 /// Traits for which total order matches that of [`ToOutput::vec`].
-pub trait ByteOrdered: ToOutput {
+pub trait ByteOrdered: ToOutput + PartialOrd {
     fn bytes_cmp(&self, other: &Self) -> Ordering {
         self.vec().cmp(&other.vec())
     }
