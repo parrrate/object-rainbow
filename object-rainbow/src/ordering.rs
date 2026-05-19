d7751b9116d945a772c2128ec26be3f7599bb3cf
parrrate <alisa@parrrate.com>
2026-05-19 22:34:36 +0000

    `ByteOrdered::bytes_cmp`

--- object-rainbow/src/ordering.rs
+++ object-rainbow/src/ordering.rs
@@ -1,3 +1,10 @@
+use std::cmp::Ordering;
+
 use crate::*;
 
-pub trait ByteOrdered: ToOutput {}
+/// Traits for which total order matches that of [`ToOutput::vec`].
+pub trait ByteOrdered: ToOutput {
+    fn bytes_cmp(&self, right: &Self) -> Ordering {
+        self.vec().cmp(&right.vec())
+    }
+}
