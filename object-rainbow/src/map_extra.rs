116921c126c9b6adf7fdc8190bb73df9418c7506
parrrate <alisa@parrrate.com>
2026-05-22 21:11:28 +0000

    `SmExtra: Ord`

--- object-rainbow/src/map_extra.rs
+++ object-rainbow/src/map_extra.rs
@@ -81,7 +81,7 @@ mod private {
     use ghost::phantom;
 
     #[phantom]
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
     pub struct SmExtra<M>;
 }
 
