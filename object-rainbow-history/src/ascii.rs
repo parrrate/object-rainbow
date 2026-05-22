d1d51e26925b1da4185bdb6d9c7c372fb72f120b
parrrate <alisa@parrrate.com>
2026-05-22 20:07:01 +0000

    `Split: Apply`

--- object-rainbow-history/src/ascii.rs
+++ object-rainbow-history/src/ascii.rs
@@ -1,1 +1,18 @@
+use crate::Apply;
+
 pub struct Split;
+
+impl<S: 'static + Send + AsRef<str>> Apply<S> for Split {
+    type Output = Vec<String>;
+
+    fn apply(
+        &mut self,
+        diff: S,
+    ) -> impl Send + Future<Output = object_rainbow::Result<Self::Output>> {
+        core::future::ready(Ok(diff
+            .as_ref()
+            .split_ascii_whitespace()
+            .map(String::from)
+            .collect()))
+    }
+}
