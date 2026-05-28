3acefabffe1d3aae5a8e1dfcced481fcf11b1a7f
parrrate <alisa@parrrate.com>
2026-05-28 21:25:29 +0000

    `DiffHashes: Debug`

--- object-rainbow/src/lib.rs
+++ object-rainbow/src/lib.rs
@@ -796,7 +796,7 @@ pub trait ParseSliceExtra<Extra: Clone>: for<'a> Parse<Input<'a, Extra>> {
 
 impl<T: for<'a> Parse<Input<'a, Extra>>, Extra: Clone> ParseSliceExtra<Extra> for T {}
 
-#[derive(ToOutput)]
+#[derive(Debug, ToOutput)]
 pub struct DiffHashes {
     pub tags: Hash,
     pub topology: Hash,
