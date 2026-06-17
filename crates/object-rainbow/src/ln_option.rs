7d8a743cdf90b08ab2980eb8c4953c527de72f3b
parrrate <alisa@parrrate.com>
2026-06-17 19:57:04 +0000

    `LnOption: ToOutput`

--- crates/object-rainbow/src/ln_option.rs
+++ crates/object-rainbow/src/ln_option.rs
@@ -1,1 +1,16 @@
+use crate::*;
+
 pub struct LnOption<T>(pub Option<T>);
+
+impl<T: ToOutput> ToOutput for LnOption<T> {
+    fn to_output(&self, output: &mut impl Output) {
+        if let Some(object) = &self.0 {
+            if output.is_real() {
+                output.write(&[0xff]);
+            }
+            object.to_output(output);
+        } else if output.is_real() {
+            output.write(&[0xfe]);
+        }
+    }
+}
