f75c2444f489d5783ca13fb03e44c637e4122a5e
parrrate <alisa@parrrate.com>
2026-06-17 19:21:27 +0000

    `Dt: Deref`

--- object-rainbow/src/default_terminated.rs
+++ object-rainbow/src/default_terminated.rs
@@ -6,12 +6,20 @@ pub struct Dt<T> {
     inner: Arc<WithRepr<T>>,
 }
 
+impl<T> Deref for Dt<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.inner.object()
+    }
+}
+
 impl<T, A: Default + InlineOutput> ToOutput for Dt<T>
 where
     for<'a> &'a T: IntoIterator<Item = &'a A>,
 {
     fn to_output(&self, output: &mut impl Output) {
-        self.inner.object().iter_to_output(output);
+        self.iter_to_output(output);
         A::default().to_output(output);
     }
 }
