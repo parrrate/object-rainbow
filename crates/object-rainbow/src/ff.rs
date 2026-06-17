1fcf3a1552dbf0dbaa3a555c44ff247f00f23d0b
parrrate <alisa@parrrate.com>
2026-06-17 20:21:49 +0000

    `Ff: ToOutput`

--- crates/object-rainbow/src/ff.rs
+++ crates/object-rainbow/src/ff.rs
@@ -2,3 +2,9 @@ use crate::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ListHashes, Topological)]
 pub struct Ff;
+
+impl ToOutput for Ff {
+    fn to_output(&self, output: &mut impl Output) {
+        output.write(&[0xff]);
+    }
+}
