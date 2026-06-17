1651ce5dd0f310e631ec58257846c7e6ca3459b6
parrrate <alisa@parrrate.com>
2026-06-17 22:50:32 +0000

    `Nt: ParseInline`

--- crates/object-rainbow/src/none_terminated.rs
+++ crates/object-rainbow/src/none_terminated.rs
@@ -2,3 +2,16 @@ use crate::*;
 
 #[derive(ParseAsInline)]
 pub struct Nt<T>(pub T);
+
+impl<T: IntoIterator<Item = A> + FromIterator<A>, A, I: ParseInput> ParseInline<I> for Nt<T>
+where
+    Option<A>: ParseInline<I>,
+{
+    fn parse_inline(input: &mut I) -> crate::Result<Self> {
+        let mut items = Vec::new();
+        while let Some(item) = input.parse_inline()? {
+            items.push(item);
+        }
+        Ok(Self(items.into_iter().collect()))
+    }
+}
