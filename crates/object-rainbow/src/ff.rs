e5205f24151a5e7800a7547120d4f84e4297094b
parrrate <alisa@parrrate.com>
2026-06-17 20:42:08 +0000

    `Ff: MaybeHasNiche`

--- crates/object-rainbow/src/ff.rs
+++ crates/object-rainbow/src/ff.rs
@@ -1,4 +1,6 @@
-use crate::*;
+use typenum::U254;
+
+use crate::{decr_byte_niche::DecrByteNiche, *};
 
 #[derive(
     Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ListHashes, Topological, ParseAsInline,
@@ -27,4 +29,14 @@ impl ByteOrd for Ff {
     fn bytes_cmp(&self, Self: &Self) -> Ordering {
         Ordering::Equal
     }
+}
+
+impl MaybeHasNiche for Ff {
+    type MnArray = SomeNiche<DecrByteNiche<U254>>;
+}
+
+#[test]
+fn ff_option() {
+    assert_eq!(Some(Ff).vec(), [0xff]);
+    assert_eq!(None::<Ff>.vec(), [0xfe]);
 }
