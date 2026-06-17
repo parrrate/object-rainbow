8d7a2653ee9996710083efac897d442cebe79ecb
parrrate <alisa@parrrate.com>
2026-06-17 20:00:06 +0000

    `LnOption: ByteOrd`

--- crates/object-rainbow/src/ln_option.rs
+++ crates/object-rainbow/src/ln_option.rs
@@ -3,6 +3,17 @@ use crate::*;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct LnOption<T>(pub Option<T>);
 
+impl<T: ByteOrd> ByteOrd for LnOption<T> {
+    fn bytes_cmp(&self, other: &Self) -> Ordering {
+        match (&self.0, &other.0) {
+            (None, None) => Ordering::Equal,
+            (None, Some(_)) => Ordering::Less,
+            (Some(_), None) => Ordering::Greater,
+            (Some(a), Some(b)) => a.bytes_cmp(b),
+        }
+    }
+}
+
 impl<T: ToOutput> ToOutput for LnOption<T> {
     fn to_output(&self, output: &mut impl Output) {
         if let Some(object) = &self.0 {
