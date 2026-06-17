6cb7e6978b5431f1c917ea8c5b97b5df95b64a59
parrrate <alisa@parrrate.com>
2026-06-17 18:47:24 +0000

    `--group-features`

--- hack.sh
+++ hack.sh
@@ -1,1 +1,3 @@
-exec cargo hack clippy --workspace --feature-powerset --lib --tests --bins --exclude xtask  --exclude xtask-release
+set -e
+cargo hack clippy --package object-rainbow --feature-powerset --group-features bitvec,bytes,cid,indexmap,ulid --lib --tests --bins
+cargo hack clippy --workspace --feature-powerset --lib --tests --bins --exclude object-rainbow --exclude xtask  --exclude xtask-release
