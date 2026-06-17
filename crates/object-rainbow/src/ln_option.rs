e6b826186aa0c0fd1ee585ca67843e6d2c45e463
parrrate <alisa@parrrate.com>
2026-06-17 19:58:50 +0000

    `LnOption: Ord`

--- crates/object-rainbow/src/ln_option.rs
+++ crates/object-rainbow/src/ln_option.rs
@@ -1,6 +1,6 @@
 use crate::*;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct LnOption<T>(pub Option<T>);
 
 impl<T: ToOutput> ToOutput for LnOption<T> {
