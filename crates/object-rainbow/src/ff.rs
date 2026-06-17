b71f3f774f300337cf3a62db75f113b7036d5d83
parrrate <alisa@parrrate.com>
2026-06-17 20:27:59 +0000

    `Ff:: Parse[Inline]`

--- crates/object-rainbow/src/ff.rs
+++ crates/object-rainbow/src/ff.rs
@@ -1,6 +1,8 @@
 use crate::*;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ListHashes, Topological)]
+#[derive(
+    Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ListHashes, Topological, ParseAsInline,
+)]
 pub struct Ff;
 
 impl ToOutput for Ff {
@@ -10,3 +12,13 @@ impl ToOutput for Ff {
 }
 
 impl InlineOutput for Ff {}
+
+impl<I: ParseInput> ParseInline<I> for Ff {
+    fn parse_inline(input: &mut I) -> crate::Result<Self> {
+        if input.parse_inline::<u8>()? == 0xff {
+            Ok(Self)
+        } else {
+            Err(Error::OutOfBounds)
+        }
+    }
+}
