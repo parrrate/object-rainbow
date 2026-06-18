e609d44a8bb63a66177668f87cab76830753c72b
parrrate <alisa@parrrate.com>
2026-06-18 01:25:41 +0000

    `&MonostateHeaders: IntoIterator`

--- crates/object-rainbow/src/monostate_headers.rs
+++ crates/object-rainbow/src/monostate_headers.rs
@@ -38,3 +38,32 @@ impl<T: FromIterator<A>, M: Monostate, A> FromIterator<A> for MonostateHeaders<T
         }
     }
 }
+
+pub struct Iter<I, M> {
+    inner: I,
+    monostate: M,
+}
+
+impl<I: Iterator, M: Monostate> Iterator for Iter<I, M> {
+    type Item = (M, I::Item);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next().map(|item| (self.monostate, item))
+    }
+}
+
+impl<'a, T, M: Monostate> IntoIterator for &'a MonostateHeaders<T, M>
+where
+    &'a T: IntoIterator,
+{
+    type Item = (M, <&'a T as IntoIterator>::Item);
+
+    type IntoIter = Iter<<&'a T as IntoIterator>::IntoIter, M>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Iter {
+            inner: self.inner.into_iter(),
+            monostate: self.monostate,
+        }
+    }
+}
