dd7ecc3678eedd10a644a091ba46e5510e1c835e
parrrate <alisa@parrrate.com>
2026-06-18 01:27:05 +0000

    `&mut MonostateHeaders: IntoIterator`

--- crates/object-rainbow/src/monostate_headers.rs
+++ crates/object-rainbow/src/monostate_headers.rs
@@ -5,12 +5,12 @@ pub struct MonostateHeaders<T, M = ()> {
     monostate: M,
 }
 
-pub struct IntoIter<I, M> {
+pub struct Iter<I, M> {
     inner: I,
     monostate: M,
 }
 
-impl<I: Iterator, M: Monostate> Iterator for IntoIter<I, M> {
+impl<I: Iterator, M: Monostate> Iterator for Iter<I, M> {
     type Item = (M, I::Item);
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -20,10 +20,10 @@ impl<I: Iterator, M: Monostate> Iterator for IntoIter<I, M> {
 
 impl<T: IntoIterator, M: Monostate> IntoIterator for MonostateHeaders<T, M> {
     type Item = (M, T::Item);
-    type IntoIter = IntoIter<T::IntoIter, M>;
+    type IntoIter = Iter<T::IntoIter, M>;
 
     fn into_iter(self) -> Self::IntoIter {
-        IntoIter {
+        Iter {
             inner: self.inner.into_iter(),
             monostate: self.monostate,
         }
@@ -39,19 +39,6 @@ impl<T: FromIterator<A>, M: Monostate, A> FromIterator<A> for MonostateHeaders<T
     }
 }
 
-pub struct Iter<I, M> {
-    inner: I,
-    monostate: M,
-}
-
-impl<I: Iterator, M: Monostate> Iterator for Iter<I, M> {
-    type Item = (M, I::Item);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.inner.next().map(|item| (self.monostate, item))
-    }
-}
-
 impl<'a, T, M: Monostate> IntoIterator for &'a MonostateHeaders<T, M>
 where
     &'a T: IntoIterator,
@@ -59,6 +46,22 @@ where
     type Item = (M, <&'a T as IntoIterator>::Item);
 
     type IntoIter = Iter<<&'a T as IntoIterator>::IntoIter, M>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Iter {
+            inner: self.inner.into_iter(),
+            monostate: self.monostate,
+        }
+    }
+}
+
+impl<'a, T, M: Monostate> IntoIterator for &'a mut MonostateHeaders<T, M>
+where
+    &'a mut T: IntoIterator,
+{
+    type Item = (M, <&'a mut T as IntoIterator>::Item);
+
+    type IntoIter = Iter<<&'a mut T as IntoIterator>::IntoIter, M>;
 
     fn into_iter(self) -> Self::IntoIter {
         Iter {
