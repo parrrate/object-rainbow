e22d4f9126701954bfa68e6b114ddc87a2cc8979
parrrate <alisa@parrrate.com>
2026-06-18 19:41:09 +0000

    `Sequence: Deref`

--- crates/object-rainbow/src/sequence.rs
+++ crates/object-rainbow/src/sequence.rs
@@ -2,3 +2,11 @@ use crate::*;
 
 #[derive(Debug, ParseAsInline, Clone, Copy, Default)]
 pub struct Sequence<T>(pub T);
+
+impl<T> Deref for Sequence<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
