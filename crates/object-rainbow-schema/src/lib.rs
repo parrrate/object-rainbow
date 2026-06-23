4c6049e3fa24e1551a1783057203f22987d458d8
parrrate <alisa@parrrate.com>
2026-06-23 21:12:07 +0000

    `Schema: MaybeHasNiche`

--- crates/object-rainbow-schema/src/lib.rs
+++ crates/object-rainbow-schema/src/lib.rs
@@ -2,11 +2,12 @@
 use std::convert::Infallible;
 use std::sync::Arc;
 
-use object_rainbow::{Enum, InlineOutput, Parse, ParseInline, Tagged, ToOutput};
+use object_rainbow::{Enum, InlineOutput, MaybeHasNiche, Parse, ParseInline, Tagged, ToOutput};
 
-#[derive(Enum, ToOutput, Parse, ParseInline)]
+#[derive(Enum, ToOutput, Parse, ParseInline, MaybeHasNiche)]
 #[enumtag("char")]
 #[parse(unchecked)]
+#[niche(tag)]
 pub enum Schema {
     Point(
         #[cfg(feature = "point")] Arc<Self>,
