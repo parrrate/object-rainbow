cdd4c7528d1af4eb3c0757d7f88d8fb63242b14c
parrrate <alisa@parrrate.com>
2026-06-25 23:59:31 +0000

    `CollectionSchema: ParseInline`

--- crates/object-rainbow-schema/src/collections.rs
+++ crates/object-rainbow-schema/src/collections.rs
@@ -1,6 +1,6 @@
 use crate::*;
 
-#[derive(Enum, ToOutput, Parse)]
+#[derive(Enum, ToOutput, Parse, ParseInline)]
 pub enum CollectionSchema {
     Amt(
         #[cfg(feature = "amt")] (Arc<InlineSchema>, Arc<InlineSchema>),
