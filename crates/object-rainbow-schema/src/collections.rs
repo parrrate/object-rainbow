d3f6cbbab90e45d005ec1ddbbcc6955f1b8f2988
parrrate <alisa@parrrate.com>
2026-06-25 23:57:32 +0000

    `CollectionSchema::Amt`

--- crates/object-rainbow-schema/src/collections.rs
+++ crates/object-rainbow-schema/src/collections.rs
@@ -1,1 +1,8 @@
-pub enum CollectionSchema {}
+use crate::*;
+
+pub enum CollectionSchema {
+    Amt(
+        #[cfg(feature = "amt")] (Arc<InlineSchema>, Arc<InlineSchema>),
+        #[cfg(not(feature = "amt"))] Infallible,
+    ),
+}
