b6b31e14d9a7ada61b2f9aefad8fb70eb4388680
parrrate <alisa@parrrate.com>
2026-06-25 19:34:22 +0000

    `AbstractSchema: Tagged`

--- crates/object-rainbow-schema/src/lib.rs
+++ crates/object-rainbow-schema/src/lib.rs
@@ -7,7 +7,7 @@ use object_rainbow::{
 #[cfg(feature = "point")]
 use object_rainbow_point::Point;
 
-pub trait AbstractSchema: ToOutput + ListHashes + Topological {
+pub trait AbstractSchema: ToOutput + ListHashes + Topological + Tagged {
     fn niche(&self) -> SchemaNiche;
 }
 
