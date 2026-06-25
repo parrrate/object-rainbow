6ca684ab5c1a8c3fd4f40d950a95c937d0cd1a3f
parrrate <alisa@parrrate.com>
2026-06-25 23:59:52 +0000

    `CollectionSchema: Topological`

--- crates/object-rainbow-schema/src/collections.rs
+++ crates/object-rainbow-schema/src/collections.rs
@@ -1,6 +1,6 @@
 use crate::*;
 
-#[derive(Enum, ToOutput, Parse, ParseInline, ListHashes)]
+#[derive(Enum, ToOutput, Parse, ParseInline, ListHashes, Topological)]
 pub enum CollectionSchema {
     Amt(
         #[cfg(feature = "amt")] (Arc<InlineSchema>, Arc<InlineSchema>),
