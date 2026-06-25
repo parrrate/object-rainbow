eae8d8b2d61e5ec1723ecd252f82c1a1e1854270
parrrate <alisa@parrrate.com>
2026-06-25 23:58:54 +0000

    un`derive` `CollectionSchema: InlineOutput`

--- crates/object-rainbow-schema/src/collections.rs
+++ crates/object-rainbow-schema/src/collections.rs
@@ -1,9 +1,11 @@
 use crate::*;
 
-#[derive(Enum, ToOutput, InlineOutput)]
+#[derive(Enum, ToOutput)]
 pub enum CollectionSchema {
     Amt(
         #[cfg(feature = "amt")] (Arc<InlineSchema>, Arc<InlineSchema>),
         #[cfg(not(feature = "amt"))] Infallible,
     ),
 }
+
+impl InlineOutput for CollectionSchema {}
