1f52f272759f9558d5abdb64a17144cee2296fab
parrrate <alisa@parrrate.com>
2026-06-25 21:26:15 +0000

    `ArraySchema: MaybeHasNiche`

--- crates/object-rainbow-schema/src/lib.rs
+++ crates/object-rainbow-schema/src/lib.rs
@@ -65,7 +65,7 @@ impl<I: PointInput<Extra = NumericSchema>> ParseInline<I> for NumericValue {
     }
 }
 
-#[derive(ToOutput, InlineOutput, Parse, ParseInline)]
+#[derive(ToOutput, InlineOutput, Parse, ParseInline, MaybeHasNiche)]
 pub struct ArraySchema {
     pub length: u64,
     pub schema: Arc<InlineSchema>,
