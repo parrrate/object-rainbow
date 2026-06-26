c442fc7207033ef0b08538fb57c4aa0ef87b4971
parrrate <alisa@parrrate.com>
2026-06-26 12:02:54 +0000

    `TupleOfArrays: Parse`

--- crates/object-rainbow/src/tuple_of_arrays.rs
+++ crates/object-rainbow/src/tuple_of_arrays.rs
@@ -1,4 +1,4 @@
-use crate::ToOutput;
+use crate::*;
 
 pub struct TupleOfArrays<A, B>(A, B);
 
@@ -6,5 +6,26 @@ impl<A: ToOutput, B: ToOutput> ToOutput for TupleOfArrays<A, B> {
     fn to_output(&self, output: &mut impl crate::Output) {
         self.0.to_output(output);
         self.1.to_output(output);
+    }
+}
+
+impl<
+    A: Parse<I> + IntoIterator<Item = Ae>,
+    B: Parse<I> + IntoIterator<Item = Be>,
+    I: ParseInput,
+    Ae: Size,
+    Be: Size,
+> Parse<I> for TupleOfArrays<A, B>
+{
+    fn parse(input: I) -> crate::Result<Self> {
+        let (mut input, n) = input.remaining()?;
+        let k = Ae::SIZE + Be::SIZE;
+        if !n.is_multiple_of(k) {
+            return Err(crate::error_parse!("doesn't divide evenly"));
+        }
+        let n = n
+            .checked_div(k)
+            .ok_or_else(|| crate::error_parse!("division failed"))?;
+        Ok(Self(input.split_parse(n * Ae::SIZE)?, input.parse()?))
     }
 }
