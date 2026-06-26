d559e774e292103e72c49b84e7511fde6ea52ac5
parrrate <alisa@parrrate.com>
2026-06-26 17:11:36 +0000

    `--exclude-features _collections-*`

--- hack.sh
+++ hack.sh
@@ -1,4 +1,4 @@
 set -e
 cargo hack clippy --package object-rainbow --feature-powerset --group-features bitvec,bytes,cid,indexmap,ulid --lib --tests --bins
 cargo hack clippy --workspace --feature-powerset --lib --tests --bins --exclude object-rainbow --exclude object-rainbow-schema --exclude xtask  --exclude xtask-release
-cargo hack clippy --package object-rainbow-schema --feature-powerset --exclude-features _collections --lib --tests --bins
+cargo hack clippy --package object-rainbow-schema --feature-powerset --exclude-features _collections,_collections-map,_collections-set --lib --tests --bins
