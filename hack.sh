da5df488f2b09201628c822e80f8f7739b986b54
parrrate <alisa@parrrate.com>
2026-06-26 02:27:36 +0000

    exclude `_collections` from `hack`

--- hack.sh
+++ hack.sh
@@ -1,3 +1,4 @@
 set -e
 cargo hack clippy --package object-rainbow --feature-powerset --group-features bitvec,bytes,cid,indexmap,ulid --lib --tests --bins
-cargo hack clippy --workspace --feature-powerset --lib --tests --bins --exclude object-rainbow --exclude xtask  --exclude xtask-release
+cargo hack clippy --workspace --feature-powerset --lib --tests --bins --exclude object-rainbow --exclude object-rainbow-schema --exclude xtask  --exclude xtask-release
+cargo hack clippy --package object-rainbow-schema --feature-powerset --exclude-features _collections --lib --tests --bins
