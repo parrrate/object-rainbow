b9d87bca33f590e878ddb8dbbd470e7853532686
parrrate <alisa@parrrate.com>
2026-06-26 22:07:57 +0000

    `EnumSchema: PartialEq`

--- crates/object-rainbow-schema/src/lib.rs
+++ crates/object-rainbow-schema/src/lib.rs
@@ -1075,7 +1075,9 @@ where
     }
 }
 
-#[derive(Debug, ToOutput, InlineOutput, Parse, ParseInline, ListHashes, Topological, Tagged)]
+#[derive(
+    Debug, ToOutput, InlineOutput, Parse, ParseInline, ListHashes, Topological, Tagged, PartialEq,
+)]
 pub struct EnumSchema<T> {
     pub kind: NumericSchema,
     pub variants: Arc<LpVec<Arc<T>>>,
