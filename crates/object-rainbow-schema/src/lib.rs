5b7c710d22e314edddb9b6a999ffa8a2b0444a02
parrrate <alisa@parrrate.com>
2026-06-26 00:23:54 +0000

    `EnumValue: ListHashes`

--- crates/object-rainbow-schema/src/lib.rs
+++ crates/object-rainbow-schema/src/lib.rs
@@ -812,7 +812,7 @@ impl<T: AbstractSchema> AbstractSchema for EnumSchema<T> {
     }
 }
 
-#[derive(ToOutput, InlineOutput)]
+#[derive(ToOutput, InlineOutput, ListHashes)]
 pub struct EnumValue<T: AbstractValue> {
     pub kind: NumericValue,
     pub variants: Arc<LpVec<Arc<T::Schema>>>,
