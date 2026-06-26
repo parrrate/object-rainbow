04f8cd88cc73c9955a50458617ebd62c31671bf9
parrrate <alisa@parrrate.com>
2026-06-26 16:10:16 +0000

    `[T; N]: PlainCollection`

--- crates/object-rainbow/src/impls/array.rs
+++ crates/object-rainbow/src/impls/array.rs
@@ -2,7 +2,10 @@ use std::ops::Mul;
 
 use typenum::ToUInt;
 
-use crate::{sequence::VecLike, *};
+use crate::{
+    sequence::{PlainCollection, VecLike},
+    *,
+};
 
 impl<T: InlineOutput, const N: usize> ToOutput for [T; N] {
     fn to_output(&self, output: &mut impl Output) {
@@ -74,4 +77,5 @@ fn byte_array_niche() {
     assert_eq!(None::<([u8; 2], bool)>.vec(), [0, 0, 2]);
 }
 
+impl<T, const N: usize> PlainCollection for [T; N] {}
 impl<T, const N: usize> VecLike for [T; N] {}
