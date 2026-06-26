13255420ff87fccc1db3f2efc21dd8635cd98294
parrrate <alisa@parrrate.com>
2026-06-26 20:18:03 +0000

    `ValueZt: ListHashes`

--- crates/object-rainbow-schema/src/lib.rs
+++ crates/object-rainbow-schema/src/lib.rs
@@ -1196,7 +1196,7 @@ impl ItemSizeSchema for TailSchema {
     }
 }
 
-#[derive(ToOutput)]
+#[derive(ToOutput, ListHashes)]
 pub struct ValueZt {
     pub schema: Extras<Arc<TailSchema>>,
     pub value: Zt<Arc<TailValue>>,
