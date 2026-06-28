4509d9894595cbbb1e88b734bd8aed33d70fcdb8
parrrate <alisa@parrrate.com>
2026-06-28 15:30:32 +0000

    `RemoteMut`

--- crates/object-rainbow/src/nested_mut.rs
+++ crates/object-rainbow/src/nested_mut.rs
@@ -1,1 +1,14 @@
+use futures_channel::oneshot;
 
+pub struct RemoteMut<'a, T> {
+    local: &'a mut T,
+    borrowed: oneshot::Receiver<T>,
+}
+
+impl<T> Drop for RemoteMut<'_, T> {
+    fn drop(&mut self) {
+        if let Ok(Some(returned)) = self.borrowed.try_recv() {
+            *self.local = returned;
+        }
+    }
+}
