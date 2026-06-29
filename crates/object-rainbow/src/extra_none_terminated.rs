12fc53762622b1175cdbafd910feb04eaba92b4f
parrrate <alisa@parrrate.com>
2026-06-29 19:59:49 +0000

    `Ent: ListHashes`

--- crates/object-rainbow/src/extra_none_terminated.rs
+++ crates/object-rainbow/src/extra_none_terminated.rs
@@ -1,6 +1,6 @@
-use crate::{extras::Extras, none_terminated::Nt};
+use crate::{extras::Extras, none_terminated::Nt, *};
 
-#[derive(Debug)]
+#[derive(Debug, ListHashes)]
 pub struct Ent<T, E = ()> {
     pub extra: Extras<E>,
     pub items: Nt<T>,
