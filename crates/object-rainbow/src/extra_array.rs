761df5ca059b8d6cef237e16b5ad16ef4fafe3c4
parrrate <alisa@parrrate.com>
2026-06-29 20:56:49 +0000

    `ExtraArray: Debug`

--- crates/object-rainbow/src/extra_array.rs
+++ crates/object-rainbow/src/extra_array.rs
@@ -1,4 +1,4 @@
 use crate::*;
 
-#[derive(ToOutput, InlineOutput, ListHashes, Topological, PartialEq)]
+#[derive(Debug, ToOutput, InlineOutput, ListHashes, Topological, PartialEq)]
 pub struct ExtraArray<T>(pub Vec<T>);
