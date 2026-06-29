27c33d80309919e7e57d7c7cbcdd11c55efdad2a
parrrate <alisa@parrrate.com>
2026-06-29 20:01:05 +0000

    `Ent: Tagged`

--- crates/object-rainbow/src/extra_none_terminated.rs
+++ crates/object-rainbow/src/extra_none_terminated.rs
@@ -1,6 +1,6 @@
 use crate::{extras::Extras, none_terminated::Nt, *};
 
-#[derive(Debug, ListHashes, Topological, Parse, ParseInline)]
+#[derive(Debug, ListHashes, Topological, Parse, ParseInline, Tagged)]
 pub struct Ent<T, E = ()> {
     pub extra: Extras<E>,
     pub items: Nt<T>,
