359fd17311473ce6075da7c5092e2f19f9245341
parrrate <alisa@parrrate.com>
2026-06-29 21:21:45 +0000

    `ExtraArray: CanonicalExtra`

--- crates/object-rainbow/src/extra_array.rs
+++ crates/object-rainbow/src/extra_array.rs
@@ -1,8 +1,16 @@
 use crate::{
-    extras::Extras, map_extra::MappedExtra, runtime_array::RuntimeArray, tuple_extra::Extra1,
+    extras::Extras, map_extra::MappedExtra, runtime_array::RuntimeArray, tuple_extra::Extra1, *,
 };
 
 pub struct ExtraArray<T, E> {
     pub extra: MappedExtra<Extras<E>, Extra1>,
     pub items: RuntimeArray<T>,
+}
+
+impl<T, E: Clone> CanonicalExtra for ExtraArray<T, E> {
+    type Extra = (u64, E);
+
+    fn canonical_extra(&self) -> Self::Extra {
+        (self.items.len() as _, self.extra.1.0.clone())
+    }
 }
