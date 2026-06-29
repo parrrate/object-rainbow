e891d6e82b10c770e41e0423994354baa9b1a199
parrrate <alisa@parrrate.com>
2026-06-29 18:12:11 +0000

    move `NtValue: ToOutput`

--- crates/object-rainbow-schema/src/lib.rs
+++ crates/object-rainbow-schema/src/lib.rs
@@ -170,15 +170,6 @@ impl AbstractCollection for NtValue {
     }
 }
 
-impl ToOutput for NtValue {
-    fn to_output(&self, output: &mut impl Output) {
-        for item in &self.items {
-            ValueOption::Some(item.clone()).to_output(output);
-        }
-        ValueOption::<InlineValue>::None(self.schema.clone()).to_output(output);
-    }
-}
-
 impl InlineOutput for NtValue {}
 impl Tagged for NtValue {}
 
--- crates/object-rainbow-schema/src/nt.rs
+++ crates/object-rainbow-schema/src/nt.rs
@@ -5,3 +5,12 @@ pub struct NtValue {
     pub items: Vec<Arc<InlineValue>>,
     pub schema: Arc<InlineSchema>,
 }
+
+impl ToOutput for NtValue {
+    fn to_output(&self, output: &mut impl Output) {
+        for item in &self.items {
+            ValueOption::Some(item.clone()).to_output(output);
+        }
+        ValueOption::<InlineValue>::None(self.schema.clone()).to_output(output);
+    }
+}
