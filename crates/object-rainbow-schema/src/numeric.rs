3e8bfe5721e3ac30b8f16840973401212c1a2a5d
parrrate <alisa@parrrate.com>
2026-06-29 17:41:36 +0000

    `numeric::NumericSchema` (move)

---
+++ crates/object-rainbow-schema/src/numeric.rs
@@ -0,0 +1,45 @@
+use crate::*;
+
+#[derive(
+    Debug,
+    Enum,
+    ToOutput,
+    Parse,
+    ParseInline,
+    MaybeHasNiche,
+    ListHashes,
+    Topological,
+    Clone,
+    PartialEq,
+)]
+#[enumtag("char")]
+#[niche(tag)]
+pub enum NumericSchema {
+    U8,
+    I8,
+    U16,
+    I16,
+    U32,
+    I32,
+    U64,
+    I64,
+    U128,
+    I128,
+    U256(Infallible),
+    I256(Infallible),
+    NzU8,
+    NzU16,
+    NzU32,
+    NzU64,
+    NzU128,
+    NzU256(Infallible),
+    F8(Infallible),
+    F16(Infallible),
+    F32,
+    F64,
+    F128(Infallible),
+    F256(Infallible),
+    OpaqueChar,
+    OpaqueBool,
+    LpU63,
+}
