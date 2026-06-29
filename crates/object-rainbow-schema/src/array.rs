90404b6a27ffabf9d154fb7b6e3971f0db35e63d
parrrate <alisa@parrrate.com>
2026-06-29 21:10:05 +0000

    `ArraySchema` tuple

--- crates/object-rainbow-schema/src/array.rs
+++ crates/object-rainbow-schema/src/array.rs
@@ -1,47 +1,32 @@
 use crate::*;
 
-#[derive(
-    Debug,
-    ToOutput,
-    InlineOutput,
-    Parse,
-    ParseInline,
-    MaybeHasNiche,
-    ListHashes,
-    Topological,
-    Tagged,
-    Clone,
-    PartialEq,
-)]
-pub struct ArraySchema {
-    pub len: u64,
-    pub schema: Arc<InlineSchema>,
-}
+pub type ArraySchema = (u64, Arc<InlineSchema>);
 
 impl AbstractSchema for ArraySchema {
     fn niche(&self) -> SchemaNiche {
-        self.schema.niche().repeat(self.len)
+        self.1.niche().repeat(self.0)
     }
 }
 
 impl DefaultSchema<ArrayValue> for ArraySchema {
     fn default_value(&self) -> Option<ArrayValue> {
         Some(ArrayValue {
-            items: std::iter::repeat_n(self.schema.default_value().map(Arc::new), self.len as _)
+            items: std::iter::repeat_n(self.1.default_value().map(Arc::new), self.0 as _)
                 .collect::<Option<_>>()?,
-            schema: Extras(self.schema.clone()),
+            schema: Extras(self.1.clone()),
         })
     }
 }
 
 impl DefaultIsMin for ArraySchema {
     fn default_is_min(&self) -> bool {
+        self.0 == 0 || self.1.default_is_min()
     }
 }
 
 impl SizeSchema for ArraySchema {
     fn size(&self) -> Option<u64> {
-        self.len.checked_mul(self.schema.size()?)
+        self.0.checked_mul(self.1.size()?)
     }
 }
 
@@ -65,7 +50,7 @@ where
     InlineValue: ParseInline<I::WithExtra<Arc<InlineSchema>>>,
 {
     fn parse_inline(input: &mut I) -> object_rainbow::Result<Self> {
-        let ArraySchema { len, schema } = input.extra().clone();
+        let (len, schema) = input.extra().clone();
         let mut items = Vec::new();
         for _ in 0..len {
             items.push(input.parse_inline_extra(schema.clone())?);
@@ -81,10 +66,7 @@ impl AbstractValue for ArrayValue {
     type Schema = ArraySchema;
 
     fn schema(&self) -> Self::Schema {
-        ArraySchema {
-            len: self.items.len() as _,
-            schema: self.schema.0.clone(),
-        }
+        (self.items.len() as _, self.schema.0.clone())
     }
 }
 
@@ -97,11 +79,5 @@ impl AbstractCollection for ArrayValue {
 impl From<ArrayValue> for InlineValue {
     fn from(value: ArrayValue) -> Self {
         Self::Array(value)
-    }
-}
-
-impl AbstractSchema for (u64, Arc<InlineSchema>) {
-    fn niche(&self) -> SchemaNiche {
-        self.1.niche().repeat(self.0)
     }
 }
