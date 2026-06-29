6eabb547e52b9e52088afff5c3efc6e77c14dd39
parrrate <alisa@parrrate.com>
2026-06-29 22:47:09 +0000

    `ExtraPoint: InlineOutput`

--- crates/object-rainbow-point/src/lib.rs
+++ crates/object-rainbow-point/src/lib.rs
@@ -931,7 +931,7 @@ impl<'a, T: FullHash> PointMut<'a, T> {
     }
 }
 
-#[derive(ToOutput)]
+#[derive(ToOutput, InlineOutput)]
 pub struct ExtraPoint<T, Extra = ()> {
     pub extra: Extras<Extra>,
     pub point: Point<T>,
