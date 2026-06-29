44475eb72475eb9c1fb443769249d1c9f43c18e8
parrrate <alisa@parrrate.com>
2026-06-29 18:51:26 +0000

    `Shared: InlineOutput`

--- crates/object-rainbow-schema/src/lib.rs
+++ crates/object-rainbow-schema/src/lib.rs
@@ -1029,7 +1029,7 @@ fn toa_seq_equiv() -> object_rainbow::Result<()> {
     Ok(())
 }
 
-#[derive(Debug, ToOutput)]
+#[derive(Debug, ToOutput, InlineOutput)]
 pub struct Shared<T>(pub Arc<T>);
 
 impl<T: AbstractValue> ExtraNoneOutput<Arc<T::Schema>> for Shared<T> {
