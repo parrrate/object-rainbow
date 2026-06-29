9826e755a4e20220f5df88858ade66738e7f54ab
parrrate <alisa@parrrate.com>
2026-06-29 18:26:49 +0000

    `ExtraNone::as_ref`

--- crates/object-rainbow/src/extra_none.rs
+++ crates/object-rainbow/src/extra_none.rs
@@ -2,3 +2,12 @@ pub enum ExtraNone<T, E = ()> {
     Some(T),
     None(E),
 }
+
+impl<T, E> ExtraNone<T, E> {
+    pub fn as_ref(&self) -> Option<&T> {
+        match self {
+            Self::Some(value) => Some(value),
+            Self::None(_) => None,
+        }
+    }
+}
