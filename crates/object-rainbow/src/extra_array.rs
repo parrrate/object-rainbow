b6a19bed9c5cd18e3eb0d68a9f562b70536260d7
parrrate <alisa@parrrate.com>
2026-06-29 20:57:58 +0000

    `ExtraArray: InlineOutput`

--- crates/object-rainbow/src/extra_array.rs
+++ crates/object-rainbow/src/extra_array.rs
@@ -2,3 +2,7 @@ use crate::*;
 
 #[derive(Debug, ToOutput, InlineOutput, ListHashes, Topological, PartialEq)]
 pub struct ExtraArray<T>(pub Vec<T>);
+
+/// Here we assume extra will match on re-parsing, which is a general requirement for
+/// correctness anyway.
+impl<T: InlineOutput> InlineOutput for ExtraArray<T> {}
