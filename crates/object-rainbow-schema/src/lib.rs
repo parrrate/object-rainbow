e36252456dc70af42ab212de0ecce6c7cd55cd94
parrrate <alisa@parrrate.com>
2026-06-29 18:51:49 +0000

    `Shared: Topological`

--- crates/object-rainbow-schema/src/lib.rs
+++ crates/object-rainbow-schema/src/lib.rs
@@ -1029,7 +1029,7 @@ fn toa_seq_equiv() -> object_rainbow::Result<()> {
     Ok(())
 }
 
-#[derive(Debug, ToOutput, InlineOutput, ListHashes)]
+#[derive(Debug, ToOutput, InlineOutput, ListHashes, Topological)]
 pub struct Shared<T>(pub Arc<T>);
 
 impl<T: AbstractValue> ExtraNoneOutput<Arc<T::Schema>> for Shared<T> {
