0494ee438da4f2371bea88758608236f7eba1243
parrrate <alisa@parrrate.com>
2026-06-29 21:02:43 +0000

    `ExtraArray: FromIterator`

--- crates/object-rainbow/src/extra_array.rs
+++ crates/object-rainbow/src/extra_array.rs
@@ -3,6 +3,12 @@ use crate::*;
 #[derive(Debug, ToOutput, InlineOutput, ListHashes, Topological, PartialEq, ParseAsInline)]
 pub struct ExtraArray<T>(pub Vec<T>);
 
+impl<A> FromIterator<A> for ExtraArray<A> {
+    fn from_iter<T: IntoIterator<Item = A>>(iter: T) -> Self {
+        Self(iter.into_iter().collect())
+    }
+}
+
 /// Here we assume extra will match on re-parsing, which is a general requirement for
 /// correctness anyway.
 impl<T: InlineOutput> InlineOutput for ExtraArray<T> {}
