894ef2540a4270199f885eae4318a5b03dbbd47d
parrrate <alisa@parrrate.com>
2026-06-30 19:43:37 +0000

    `InlineDynamic: Debug`

--- crates/object-rainbow-schema/src/dynamic.rs
+++ crates/object-rainbow-schema/src/dynamic.rs
@@ -4,4 +4,5 @@ use object_rainbow::{inline_extra::InlineExtra, map_extra::MappedExtra};
 
 use crate::{InlineSchema, InlineValue};
 
+#[derive(Debug)]
 pub struct InlineDynamic(pub MappedExtra<Arc<InlineSchema>, InlineExtra<Arc<InlineValue>>>);
