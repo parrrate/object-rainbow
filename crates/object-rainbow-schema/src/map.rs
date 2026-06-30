9b2452c5ee91881030660db664282da98e93d974
parrrate <alisa@parrrate.com>
2026-06-30 19:37:15 +0000

    `InlineMap: Map`

--- crates/object-rainbow-schema/src/map.rs
+++ crates/object-rainbow-schema/src/map.rs
@@ -1,3 +1,19 @@
+use std::sync::Arc;
+
+use object_rainbow::map_extra::Map;
+
+use crate::InlineValue;
+
 pub enum InlineMap {
     I,
+}
+
+impl Map<Arc<InlineValue>> for InlineMap {
+    type Mapped = Arc<InlineValue>;
+
+    fn map(&self, value: Arc<InlineValue>) -> Self::Mapped {
+        match self {
+            Self::I => value,
+        }
+    }
 }
