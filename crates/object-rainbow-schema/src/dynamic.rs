c82333242d367d26d652d9af3a8622761f31dd93
parrrate <alisa@parrrate.com>
2026-06-30 19:44:49 +0000

    `InlineDynamic: ListHashes`

--- crates/object-rainbow-schema/src/dynamic.rs
+++ crates/object-rainbow-schema/src/dynamic.rs
@@ -1,12 +1,13 @@
 use std::sync::Arc;
 
 use object_rainbow::{
-    InlineOutput, ToOutput, inline_extra::InlineExtra, map_extra::MappedExtra, tuple_extra::Extra0,
+    InlineOutput, ListHashes, ToOutput, inline_extra::InlineExtra, map_extra::MappedExtra,
+    tuple_extra::Extra0,
 };
 
 use crate::{InlineSchema, InlineValue};
 
-#[derive(Debug, ToOutput, InlineOutput)]
+#[derive(Debug, ToOutput, InlineOutput, ListHashes)]
 pub struct InlineDynamic(
     pub MappedExtra<MappedExtra<Arc<InlineSchema>, Extra0>, InlineExtra<Arc<InlineValue>>>,
 );
