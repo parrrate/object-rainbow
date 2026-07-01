4a849e4239da380df0c9e2b199f7d54a5ebaf243
parrrate <alisa@parrrate.com>
2026-07-01 17:35:55 +0000

    `InlineMap::Point`

--- crates/object-rainbow-schema/src/map.rs
+++ crates/object-rainbow-schema/src/map.rs
@@ -4,13 +4,16 @@ use object_rainbow::{
     Enum, InlineOutput, ListHashes, Parse, ParseInline, Tagged, ToOutput, Topological,
     extra_option::ExtraOption, map_extra::TryMap,
 };
+use object_rainbow_point::Point;
 
 use crate::{AsMap, InlineValue, IsUnit, dynamic::InlineDynamic};
 
 #[derive(
     Enum, Debug, ToOutput, InlineOutput, ListHashes, Topological, Parse, ParseInline, PartialEq,
 )]
+#[topology(recursive)]
 pub enum InlineMap {
+    Point(Point<Self>),
     I,
     K1(InlineDynamic),
     K,
@@ -23,6 +26,11 @@ impl TryMap<Arc<InlineValue>> for InlineMap {
 
     fn map(&self, value: Arc<InlineValue>) -> object_rainbow::Result<Self::Mapped> {
         Ok(match self {
+            Self::Point(_) => {
+                return Err(object_rainbow::error_operation!(
+                    "point can only map in async"
+                ));
+            }
             Self::I => value,
             Self::K1(value) => value.value(),
             Self::K => Arc::new(InlineValue::Map(Arc::new(Self::K1(InlineDynamic::new(
