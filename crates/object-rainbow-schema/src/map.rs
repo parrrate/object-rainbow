186c6f23f6221e120fc254d3f2dcefa4930698bf
parrrate <alisa@parrrate.com>
2026-07-01 17:22:58 +0000

    `InlineValue: AsMap`

--- crates/object-rainbow-schema/src/map.rs
+++ crates/object-rainbow-schema/src/map.rs
@@ -1,10 +1,11 @@
 use std::sync::Arc;
 
 use object_rainbow::{
-    Enum, InlineOutput, ListHashes, Parse, ParseInline, ToOutput, Topological, map_extra::TryMap,
+    Enum, InlineOutput, ListHashes, Parse, ParseInline, ToOutput, Topological,
+    extra_option::ExtraOption, map_extra::TryMap,
 };
 
-use crate::{InlineValue, dynamic::InlineDynamic};
+use crate::{AsMap, InlineValue, IsUnit, dynamic::InlineDynamic};
 
 #[derive(
     Enum, Debug, ToOutput, InlineOutput, ListHashes, Topological, Parse, ParseInline, PartialEq,
@@ -26,5 +27,25 @@ impl TryMap<Arc<InlineValue>> for InlineMap {
                 value,
             ))))),
         })
+    }
+}
+
+impl AsMap<Arc<InlineMap>> for InlineValue {
+    fn as_map(&self) -> object_rainbow::Result<Arc<InlineMap>> {
+        match self {
+            Self::Option(ExtraOption::Some(value)) => value.as_map(),
+            Self::Concat(a, b) if b.is_unit() => a.as_map(),
+            Self::Concat(a, b) if a.is_unit() => b.as_map(),
+            Self::Array(array)
+                if let Some(first) = array.items.first()
+                    && array.items.len() == 1
+                    && let only = first =>
+            {
+                only.as_map()
+            }
+            Self::Enum(value) => value.value.as_map(),
+            Self::Map(map) => Ok(map.clone()),
+            _ => Err(object_rainbow::error_operation!("not a map")),
+        }
     }
 }
