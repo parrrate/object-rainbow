478466e9454d6d8ca99b34e2ac141775eb1ba22c
parrrate <alisa@parrrate.com>
2026-07-21 16:02:35 +0000

    `AmtSet::replace` on `Apply<((), T)>`

--- crates/object-rainbow-apply/src/amt.rs
+++ crates/object-rainbow-apply/src/amt.rs
@@ -85,10 +85,10 @@ impl<T: Component> Apply<T> for AmtSet<T> {
 }
 
 impl<T: Component> Apply<((), T)> for AmtSet<T> {
-    type Output = bool;
+    type Output = Option<T>;
 
     async fn apply(&mut self, ((), value): ((), T)) -> object_rainbow::Result<Self::Output> {
-        self.insert(value).await
+        self.replace(value).await
     }
 }
 
