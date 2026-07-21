439b6a08ec9431beea57fa145160ab8c5cda08cf
parrrate <alisa@parrrate.com>
2026-07-21 16:26:14 +0000

    `Inserted: ParseInline`

--- crates/object-rainbow-apply/src/unique_diffs.rs
+++ crates/object-rainbow-apply/src/unique_diffs.rs
@@ -7,7 +7,9 @@ use object_rainbow_hamt::HamtSet;
 
 use crate::{Apply, Parallel, Sequential, skip::FilterDiffs};
 
-#[derive(Debug, Clone, Copy, ToOutput, InlineOutput, Tagged, ListHashes, Topological, Parse)]
+#[derive(
+    Debug, Clone, Copy, ToOutput, InlineOutput, Tagged, ListHashes, Topological, Parse, ParseInline,
+)]
 pub struct Inserted;
 
 impl Apply<bool> for Inserted {
