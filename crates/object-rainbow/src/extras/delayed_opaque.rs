fb436e08caf263e39bc3b45bf44a7b85cf50cc6a
parrrate <alisa@parrrate.com>
2026-07-30 23:26:48 +0000

    `ParseDelayedOpaqueInline: ParseDelayedOpaque`

--- crates/object-rainbow/src/extras/delayed_opaque.rs
+++ crates/object-rainbow/src/extras/delayed_opaque.rs
@@ -4,7 +4,7 @@ pub trait ParseDelayedOpaque<E: 'static + Clone>: Sized {
     fn parse_delayed_opaque<I: PointInput<Extra: Fetch<T = E>>>(input: I) -> Result<Self>;
 }
 
-pub trait ParseDelayedOpaqueInline<E: 'static + Clone>: Sized {
+pub trait ParseDelayedOpaqueInline<E: 'static + Clone>: ParseDelayedOpaque<E> {
     fn parse_delayed_opaque_inline<I: PointInput<Extra: Fetch<T = E>>>(
         input: &mut I,
     ) -> Result<Self>;
