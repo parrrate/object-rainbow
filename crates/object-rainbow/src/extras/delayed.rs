06636328a2c8ce56843ebc11d5763d206112e044
parrrate <alisa@parrrate.com>
2026-07-30 23:01:37 +0000

    `DelayedExtra: FetchBytes`

--- crates/object-rainbow/src/extras/delayed.rs
+++ crates/object-rainbow/src/extras/delayed.rs
@@ -1,1 +1,13 @@
+use crate::FetchBytes;
+
 pub struct DelayedExtra<E, F>(pub E, pub F);
+
+impl<E, F: FetchBytes> FetchBytes for DelayedExtra<E, F> {
+    fn fetch_bytes(&'_ self) -> crate::FailFuture<'_, crate::ByteNode> {
+        self.1.fetch_bytes()
+    }
+
+    fn fetch_data(&'_ self) -> crate::FailFuture<'_, Vec<u8>> {
+        self.1.fetch_data()
+    }
+}
