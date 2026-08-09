352229288741df2f1efaf2014f80529e188979ed
parrrate <alisa@parrrate.com>
2026-08-09 15:17:25 +0000

    `AssertRefless: Ord`

--- crates/object-rainbow/src/refless/assert_refless.rs
+++ crates/object-rainbow/src/refless/assert_refless.rs
@@ -1,6 +1,6 @@
 use crate::*;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct AssertRefless<T>(pub T);
 
 impl<T> ListHashes for AssertRefless<T> {}
