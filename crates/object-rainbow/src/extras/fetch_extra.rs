21fab7a249587643f6cef7e6bd91d815d77ae1c9
parrrate <alisa@parrrate.com>
2026-08-09 15:51:44 +0000

    `FetchExtra: Size`

--- crates/object-rainbow/src/extras/fetch_extra.rs
+++ crates/object-rainbow/src/extras/fetch_extra.rs
@@ -12,7 +12,7 @@ pub trait ParseFetchInline<E: 'static + Clone>: ParseFetch<E> {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, ToOutput, Tagged, ListHashes, Topological)]
+#[derive(Debug, Clone, PartialEq, Eq, ToOutput, Tagged, ListHashes, Topological, Size)]
 pub struct FetchExtra<F>(pub F);
 
 impl<
