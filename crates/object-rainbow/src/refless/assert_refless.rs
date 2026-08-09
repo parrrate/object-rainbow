7bd8e01608be8b116ede5abed6d1033a57d7b58d
parrrate <alisa@parrrate.com>
2026-08-09 15:17:44 +0000

    `AssertRefless: ToOutput`

--- crates/object-rainbow/src/refless/assert_refless.rs
+++ crates/object-rainbow/src/refless/assert_refless.rs
@@ -1,6 +1,6 @@
 use crate::*;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, ToOutput)]
 pub struct AssertRefless<T>(pub T);
 
 impl<T> ListHashes for AssertRefless<T> {}
