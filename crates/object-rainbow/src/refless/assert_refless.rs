2b02de71a22befcb248b7e87d7d905ea3c418cad
parrrate <alisa@parrrate.com>
2026-08-09 15:15:26 +0000

    `AssertRefless: Parse`

--- crates/object-rainbow/src/refless/assert_refless.rs
+++ crates/object-rainbow/src/refless/assert_refless.rs
@@ -1,6 +1,15 @@
-use crate::{ListHashes, Topological};
+use crate::*;
 
 pub struct AssertRefless<T>(pub T);
 
 impl<T> ListHashes for AssertRefless<T> {}
 impl<T> Topological for AssertRefless<T> {}
+
+impl<T, I: ParseInput> Parse<I> for AssertRefless<T>
+where
+    T: for<'r> Parse<ReflessInput<'r>>,
+{
+    fn parse(input: I) -> crate::Result<Self> {
+        input.parse_refless()
+    }
+}
