b5942ffc476150b3c5abe9ee8964cc168a173b44
parrrate <alisa@parrrate.com>
2026-08-09 15:17:59 +0000

    `AssertRefless: Tagged`

--- crates/object-rainbow/src/refless/assert_refless.rs
+++ crates/object-rainbow/src/refless/assert_refless.rs
@@ -1,6 +1,6 @@
 use crate::*;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, ToOutput)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, ToOutput, Tagged)]
 pub struct AssertRefless<T>(pub T);
 
 impl<T> ListHashes for AssertRefless<T> {}
