9e9a81bff35725c172a09aea31bb553d9a8f9be6
parrrate <alisa@parrrate.com>
2026-08-09 15:50:55 +0000

    `FetchExtra: ToOutput`

--- crates/object-rainbow/src/extras/fetch_extra.rs
+++ crates/object-rainbow/src/extras/fetch_extra.rs
@@ -12,7 +12,7 @@ pub trait ParseFetchInline<E: 'static + Clone>: ParseFetch<E> {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, ToOutput)]
 pub struct FetchExtra<F>(pub F);
 
 impl<
