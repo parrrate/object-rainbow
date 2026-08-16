3a9f190c50cac597c06e683abc808a35fa37cd72
parrrate <alisa@parrrate.com>
2026-08-16 23:25:28 +0000

    `FnFetch::new`

--- crates/object-rainbow/src/fn_fetch.rs
+++ crates/object-rainbow/src/fn_fetch.rs
@@ -1,3 +1,11 @@
+use crate::*;
+
 pub struct FnFetch<F> {
     pub fetch: F,
+}
+
+impl<F: Fn() -> Fut, Fut: Future<Output: ToOutput + Traversible>> FnFetch<F> {
+    pub fn new(fetch: F) -> Self {
+        Self { fetch }
+    }
 }
