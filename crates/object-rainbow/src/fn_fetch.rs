484f9704814886d0756c6a791abee61856e4dce5
parrrate <alisa@parrrate.com>
2026-08-16 23:30:24 +0000

    `FnFetch::fetch_node`

--- crates/object-rainbow/src/fn_fetch.rs
+++ crates/object-rainbow/src/fn_fetch.rs
@@ -12,4 +12,10 @@ impl<F: Fn() -> Fut, Fut: Future<Output = Result<T>>, T: Traversible> FnFetch<F>
     pub async fn fetch(&self) -> Result<T> {
         (self.fetch)().await
     }
+
+    pub async fn fetch_node(&self) -> Result<Node<T>> {
+        let object = self.fetch().await?;
+        let resolve = object.to_resolve();
+        Ok((object, resolve))
+    }
 }
