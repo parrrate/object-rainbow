85b2dc28b6a36bf085abc012e6a826e0765cf987
parrrate <alisa@parrrate.com>
2026-08-16 23:26:06 +0000

    fix bounds

--- crates/object-rainbow/src/fn_fetch.rs
+++ crates/object-rainbow/src/fn_fetch.rs
@@ -4,7 +4,7 @@ pub struct FnFetch<F> {
     pub fetch: F,
 }
 
-impl<F: Fn() -> Fut, Fut: Future<Output: ToOutput + Traversible>> FnFetch<F> {
+impl<F: Fn() -> Fut, Fut: Future<Output: Traversible>> FnFetch<F> {
     pub fn new(fetch: F) -> Self {
         Self { fetch }
     }
