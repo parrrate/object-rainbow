ca7d9c9fa4fd562f3f48c54b940cd8c2a404635f
parrrate <alisa@parrrate.com>
2026-08-16 23:28:22 +0000

    `FnFetch::fetch()`

--- crates/object-rainbow/src/fn_fetch.rs
+++ crates/object-rainbow/src/fn_fetch.rs
@@ -8,4 +8,8 @@ impl<F: Fn() -> Fut, Fut: Future<Output = Result<T>>, T: Traversible> FnFetch<F>
     pub fn new(fetch: F) -> Self {
         Self { fetch }
     }
+
+    pub async fn fetch(&self) -> Result<T> {
+        (self.fetch)().await
+    }
 }
