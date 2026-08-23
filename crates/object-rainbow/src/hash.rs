c2f68e036eb23eeea5ce92d4eb53150e6c69dda2
parrrate <alisa@parrrate.com>
2026-08-23 19:59:22 +0000

    `OptionalHash::fmt` via `Hash`

--- crates/object-rainbow/src/hash.rs
+++ crates/object-rainbow/src/hash.rs
@@ -123,10 +123,8 @@ pub struct OptionalHash([u8; HASH_SIZE]);
 
 impl Display for OptionalHash {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.is_some() {
-            for x in self.0 {
-                write!(f, "{x:X}")?;
-            }
+        if let Some(hash) = self.get() {
+            write!(f, "{hash}")?;
         } else {
             write!(f, "NONE")?;
         }
