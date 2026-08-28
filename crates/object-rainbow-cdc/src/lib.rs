99825d9b49d004494130a52cd626e3baaba43a03
parrrate <alisa@parrrate.com>
2026-08-28 20:58:52 +0000

    `Chunks: Topological`

--- crates/object-rainbow-cdc/src/lib.rs
+++ crates/object-rainbow-cdc/src/lib.rs
@@ -17,7 +17,7 @@ use static_assertions::const_assert_eq;
 #[cfg(feature = "fs")]
 mod fs;
 
-#[derive(ToOutput, Tagged, ListHashes)]
+#[derive(ToOutput, Tagged, ListHashes, Topological)]
 pub struct Chunks {
     chunks: Vec<Chunk>,
 }
