db35382bc5a10e9e2c8ebb3f65db0d1c224c7a93
parrrate <alisa@parrrate.com>
2026-08-28 20:59:17 +0000

    `Chunks: Parse`

--- crates/object-rainbow-cdc/src/lib.rs
+++ crates/object-rainbow-cdc/src/lib.rs
@@ -17,7 +17,7 @@ use static_assertions::const_assert_eq;
 #[cfg(feature = "fs")]
 mod fs;
 
-#[derive(ToOutput, Tagged, ListHashes, Topological)]
+#[derive(ToOutput, Tagged, ListHashes, Topological, Parse)]
 pub struct Chunks {
     chunks: Vec<Chunk>,
 }
