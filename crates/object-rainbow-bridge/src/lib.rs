13ec84ec2a3a7dda1a25d0fa57e31f861c66e3a7
parrrate <alisa@parrrate.com>
2026-08-31 16:05:49 +0000

    `Provide::Publish`

--- crates/object-rainbow-bridge/src/lib.rs
+++ crates/object-rainbow-bridge/src/lib.rs
@@ -1,5 +1,9 @@
+use object_rainbow::Hash;
+
 /// Commands coming from a consumer.
 pub enum Consume {}
 
 /// Responses coming from a provider.
-pub enum Provide {}
+pub enum Provide {
+    Publish(Hash),
+}
