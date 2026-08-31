4aa2fe4b3f7b896c9b326ed68120dd6b4ccba369
parrrate <alisa@parrrate.com>
2026-08-31 23:05:37 +0000

    `write_dir` create `dir`

--- crates/object-rainbow-cdc/src/walkdir.rs
+++ crates/object-rainbow-cdc/src/walkdir.rs
@@ -63,9 +63,19 @@ impl Chunks {
         dir: impl AsRef<Path>,
         map: AmtMap<Zt<String>, Option<Point<Self>>>,
     ) -> object_rainbow::Result<()> {
+        let dir = dir.as_ref();
+        if async_fs::metadata(dir).await.map(|_| false).or_else(|e| {
+            if e.kind() == std::io::ErrorKind::NotFound {
+                Ok(true)
+            } else {
+                Err(e)
+            }
+        })? {
+            async_fs::create_dir(dir).await?;
+        }
         map.stream()
             .try_for_each_concurrent(None, async |(path, chunks)| {
-                let path = dir.as_ref().join(&*path);
+                let path = dir.join(&*path);
                 if let Some(chunks) = chunks {
                     chunks.fetch().await?.to_file(path).await?;
                 } else {
