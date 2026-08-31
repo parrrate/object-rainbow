74b055c0c0abb10388b107d600bd06a4c3b39c52
parrrate <alisa@parrrate.com>
2026-08-31 17:49:17 +0000

    `consume` `recv` `Send`

--- crates/object-rainbow-bridge/src/lib.rs
+++ crates/object-rainbow-bridge/src/lib.rs
@@ -26,7 +26,7 @@ pub enum Provide {
 
 pub fn consume<E1, E2>(
     send: impl Sink<Consume, Error = E1>,
-    recv: impl Stream<Item = Result<Provide, E2>>,
+    recv: impl Send + Stream<Item = Result<Provide, E2>>,
 ) -> impl Stream<Item = object_rainbow::Result<(Arc<dyn Singular>, Vec<u8>)>> {
     try_stream(async move |co| {
         let _ = send;
