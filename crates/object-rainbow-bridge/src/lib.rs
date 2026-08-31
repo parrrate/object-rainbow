0267ab8d78e600a8847ef88e257a7711a6a6c7c1
parrrate <alisa@parrrate.com>
2026-08-31 17:21:34 +0000

    `Consume::Fetch`

--- crates/object-rainbow-bridge/src/lib.rs
+++ crates/object-rainbow-bridge/src/lib.rs
@@ -2,6 +2,7 @@ use object_rainbow::{Address, Hash};
 
 /// Commands coming from a consumer.
 pub enum Consume {
+    Fetch(Hash),
     Inc(Hash),
     Dec(Hash),
     IncChild { parent: Hash, child: Address },
