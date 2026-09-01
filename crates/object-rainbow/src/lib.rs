785c5fce50618980619bdc99d675c0ddf7d79d36
parrrate <alisa@parrrate.com>
2026-09-01 11:33:40 +0000

    `Topological::to_resolve` (move)

--- crates/object-rainbow/src/lib.rs
+++ crates/object-rainbow/src/lib.rs
@@ -796,6 +796,114 @@ pub trait Topological: ListHashes {
         self.traverse(&mut topology);
         topology
     }
+
+    fn to_resolve(&self) -> Arc<dyn Resolve> {
+        struct ByTopology {
+            topology: TopoVec,
+            topology_hash: Hash,
+        }
+
+        impl Drop for ByTopology {
+            fn drop(&mut self) {
+                while let Some(singular) = self.topology.pop() {
+                    if let Some(resolve) = singular.try_unwrap_resolve()
+                        && let Some(topology) = &mut resolve.into_topovec()
+                    {
+                        self.topology.append(topology);
+                    }
+                }
+            }
+        }
+
+        impl ByTopology {
+            fn try_resolve(&'_ self, address: Address) -> Result<FailFuture<'_, ByteNode>> {
+                let point = self
+                    .topology
+                    .get(address.index)
+                    .ok_or(Error::AddressOutOfBounds)?;
+                if point.hash() != address.hash {
+                    Err(Error::ResolutionMismatch)
+                } else {
+                    Ok(point.fetch_bytes())
+                }
+            }
+
+            fn try_resolve_data(&'_ self, address: Address) -> Result<FailFuture<'_, Vec<u8>>> {
+                let point = self
+                    .topology
+                    .get(address.index)
+                    .ok_or(Error::AddressOutOfBounds)?;
+                if point.hash() != address.hash {
+                    Err(Error::ResolutionMismatch)
+                } else {
+                    Ok(point.fetch_data())
+                }
+            }
+        }
+
+        impl Resolve for ByTopology {
+            fn resolve(
+                &'_ self,
+                address: Address,
+                _: &Arc<dyn Resolve>,
+            ) -> FailFuture<'_, ByteNode> {
+                self.try_resolve(address)
+                    .map_err(Err)
+                    .map_err(ready)
+                    .map_err(Box::pin)
+                    .unwrap_or_else(|x| x)
+            }
+
+            fn resolve_data(&'_ self, address: Address) -> FailFuture<'_, Vec<u8>> {
+                self.try_resolve_data(address)
+                    .map_err(Err)
+                    .map_err(ready)
+                    .map_err(Box::pin)
+                    .unwrap_or_else(|x| x)
+            }
+
+            fn try_resolve_local(
+                &self,
+                address: Address,
+                _: &Arc<dyn Resolve>,
+            ) -> Result<Option<ByteNode>> {
+                let point = self
+                    .topology
+                    .get(address.index)
+                    .ok_or(Error::AddressOutOfBounds)?;
+                if point.hash() != address.hash {
+                    Err(Error::ResolutionMismatch)
+                } else {
+                    point.fetch_bytes_local()
+                }
+            }
+
+            fn topology_hash(&self) -> Option<Hash> {
+                Some(self.topology_hash)
+            }
+
+            fn into_topovec(self: Arc<Self>) -> Option<TopoVec> {
+                Arc::try_unwrap(self)
+                    .ok()
+                    .as_mut()
+                    .map(|Self { topology, .. }| std::mem::take(topology))
+            }
+        }
+
+        let topology = self.topology();
+        let topology_hash = topology.data_hash();
+        for singular in &topology {
+            if let Some(resolve) = singular.as_resolve()
+                && resolve.topology_hash() == Some(topology_hash)
+            {
+                return resolve.clone();
+            }
+        }
+        Arc::new(ByTopology {
+            topology,
+            topology_hash,
+        })
+    }
 }
 
 pub trait Tagged {
@@ -914,114 +1022,6 @@ pub trait DefaultHash: FullHash + Default {
 impl<T: FullHash + Default> DefaultHash for T {}
 
 pub trait Traversible: 'static + Sized + Send + Sync + FullHash + Topological {
-    fn to_resolve(&self) -> Arc<dyn Resolve> {
-        struct ByTopology {
-            topology: TopoVec,
-            topology_hash: Hash,
-        }
-
-        impl Drop for ByTopology {
-            fn drop(&mut self) {
-                while let Some(singular) = self.topology.pop() {
-                    if let Some(resolve) = singular.try_unwrap_resolve()
-                        && let Some(topology) = &mut resolve.into_topovec()
-                    {
-                        self.topology.append(topology);
-                    }
-                }
-            }
-        }
-
-        impl ByTopology {
-            fn try_resolve(&'_ self, address: Address) -> Result<FailFuture<'_, ByteNode>> {
-                let point = self
-                    .topology
-                    .get(address.index)
-                    .ok_or(Error::AddressOutOfBounds)?;
-                if point.hash() != address.hash {
-                    Err(Error::ResolutionMismatch)
-                } else {
-                    Ok(point.fetch_bytes())
-                }
-            }
-
-            fn try_resolve_data(&'_ self, address: Address) -> Result<FailFuture<'_, Vec<u8>>> {
-                let point = self
-                    .topology
-                    .get(address.index)
-                    .ok_or(Error::AddressOutOfBounds)?;
-                if point.hash() != address.hash {
-                    Err(Error::ResolutionMismatch)
-                } else {
-                    Ok(point.fetch_data())
-                }
-            }
-        }
-
-        impl Resolve for ByTopology {
-            fn resolve(
-                &'_ self,
-                address: Address,
-                _: &Arc<dyn Resolve>,
-            ) -> FailFuture<'_, ByteNode> {
-                self.try_resolve(address)
-                    .map_err(Err)
-                    .map_err(ready)
-                    .map_err(Box::pin)
-                    .unwrap_or_else(|x| x)
-            }
-
-            fn resolve_data(&'_ self, address: Address) -> FailFuture<'_, Vec<u8>> {
-                self.try_resolve_data(address)
-                    .map_err(Err)
-                    .map_err(ready)
-                    .map_err(Box::pin)
-                    .unwrap_or_else(|x| x)
-            }
-
-            fn try_resolve_local(
-                &self,
-                address: Address,
-                _: &Arc<dyn Resolve>,
-            ) -> Result<Option<ByteNode>> {
-                let point = self
-                    .topology
-                    .get(address.index)
-                    .ok_or(Error::AddressOutOfBounds)?;
-                if point.hash() != address.hash {
-                    Err(Error::ResolutionMismatch)
-                } else {
-                    point.fetch_bytes_local()
-                }
-            }
-
-            fn topology_hash(&self) -> Option<Hash> {
-                Some(self.topology_hash)
-            }
-
-            fn into_topovec(self: Arc<Self>) -> Option<TopoVec> {
-                Arc::try_unwrap(self)
-                    .ok()
-                    .as_mut()
-                    .map(|Self { topology, .. }| std::mem::take(topology))
-            }
-        }
-
-        let topology = self.topology();
-        let topology_hash = topology.data_hash();
-        for singular in &topology {
-            if let Some(resolve) = singular.as_resolve()
-                && resolve.topology_hash() == Some(topology_hash)
-            {
-                return resolve.clone();
-            }
-        }
-        Arc::new(ByTopology {
-            topology,
-            topology_hash,
-        })
-    }
-
     fn local_fetch(self) -> Arc<dyn Fetch<T = Self>>
     where
         Self: Clone,
