c7aa9e67d4fd6977930cbb117090106ea4e68edd
parrrate <alisa@parrrate.com>
2026-09-01 18:13:46 +0000

    `DirEntry` (rename)

--- crates/object-rainbow-dirtree/src/lib.rs
+++ crates/object-rainbow-dirtree/src/lib.rs
@@ -4,7 +4,7 @@ use object_rainbow::Enum;
 use object_rainbow_amt::AmtMap;
 
 #[derive(Debug, Enum)]
-pub enum DirTree<Segment, File, Directory = ()> {
+pub enum DirEntry<Segment, File, Directory = ()> {
     File(File),
     Directory {
         directory: Directory,
