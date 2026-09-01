1d532e8e5ce4111bb8ef4452c4b0f2a41ae878bc
parrrate <alisa@parrrate.com>
2026-09-01 18:13:04 +0000

    `DirTree`

--- crates/object-rainbow-dirtree/src/lib.rs
+++ crates/object-rainbow-dirtree/src/lib.rs
@@ -1,1 +1,11 @@
+use std::sync::Arc;
 
+use object_rainbow_amt::AmtMap;
+
+pub enum DirTree<Segment, File, Directory = ()> {
+    File(File),
+    Directory {
+        directory: Directory,
+        children: AmtMap<Segment, Arc<Self>>,
+    },
+}
