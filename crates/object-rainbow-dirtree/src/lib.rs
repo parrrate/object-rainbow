7734861f59b2456c45e0437d37f348e82785c80b
parrrate <alisa@parrrate.com>
2026-09-01 18:30:01 +0000

    `DirEntry: ToOutput`

--- crates/object-rainbow-dirtree/src/lib.rs
+++ crates/object-rainbow-dirtree/src/lib.rs
@@ -1,13 +1,27 @@
 use std::sync::Arc;
 
-use object_rainbow::Enum;
+use object_rainbow::{Enum, InlineOutput, ToOutput, assert_impl};
 use object_rainbow_amt::AmtMap;
 
-#[derive(Debug, Enum)]
+#[derive(Debug, Enum, ToOutput)]
+#[output(unchecked)]
+#[output(bound = "Segment: InlineOutput")]
+#[output(bound = "File: ToOutput")]
+#[output(bound = "Directory: ToOutput")]
 pub enum DirEntry<Segment, File, Directory = ()> {
     File(File),
     Directory {
-        directory: Directory,
         children: AmtMap<Segment, Arc<Self>>,
+        directory: Directory,
     },
 }
+
+assert_impl!(
+    impl<Segment, File, Directory> ToOutput for DirEntry<Segment, File, Directory>
+    where
+        Segment: InlineOutput,
+        File: InlineOutput,
+        Directory: InlineOutput,
+    {
+    }
+);
