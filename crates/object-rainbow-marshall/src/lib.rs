385cefb058324bfc38f6d8c3aa603bd4102ed852
parrrate <alisa@parrrate.com>
2026-09-02 23:18:55 +0000

    `MarshalledDiffs: ListHashes`

--- crates/object-rainbow-marshall/src/lib.rs
+++ crates/object-rainbow-marshall/src/lib.rs
@@ -355,7 +355,7 @@ impl<T> Deref for Marshalled<T> {
 }
 
 #[cfg(feature = "apply")]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, ToOutput, Tagged)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, ToOutput, Tagged, ListHashes)]
 pub struct MarshalledDiffs<T>(pub T);
 
 #[cfg(feature = "apply")]
