02a74f57014b3f1b6764f58527d7fd16283a6dec
parrrate <alisa@parrrate.com>
2026-09-02 23:19:05 +0000

    `MarshalledDiffs: Topological`

--- crates/object-rainbow-marshall/src/lib.rs
+++ crates/object-rainbow-marshall/src/lib.rs
@@ -355,7 +355,7 @@ impl<T> Deref for Marshalled<T> {
 }
 
 #[cfg(feature = "apply")]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, ToOutput, Tagged, ListHashes)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, ToOutput, Tagged, ListHashes, Topological)]
 pub struct MarshalledDiffs<T>(pub T);
 
 #[cfg(feature = "apply")]
