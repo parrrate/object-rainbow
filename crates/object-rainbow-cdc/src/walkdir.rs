37c45d867ee87773a326eae6afd5d419bb174dbc
parrrate <alisa@parrrate.com>
2026-09-02 00:12:01 +0000

    generalise `Chunks::write_dir`

--- crates/object-rainbow-cdc/src/walkdir.rs
+++ crates/object-rainbow-cdc/src/walkdir.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 
 use async_walkdir::WalkDir;
 use futures_util::{FutureExt, StreamExt, TryStreamExt};
-use object_rainbow::{Fetch, zero_terminated::Zt};
+use object_rainbow::zero_terminated::Zt;
 use object_rainbow_point::IntoPoint;
 
 use crate::{Chunks, amt::FileMap};
@@ -54,30 +54,5 @@ impl Chunks {
             .into_iter()
             .collect();
         Ok(map)
-    }
-
-    pub async fn write_dir(dir: impl AsRef<Path>, map: FileMap) -> object_rainbow::Result<()> {
-        let dir = dir.as_ref();
-        if async_fs::metadata(dir).await.map(|_| false).or_else(|e| {
-            if e.kind() == std::io::ErrorKind::NotFound {
-                Ok(true)
-            } else {
-                Err(e)
-            }
-        })? {
-            async_fs::create_dir(dir).await?;
-        }
-        map.stream()
-            .try_for_each_concurrent(None, async |(path, chunks)| {
-                let path = dir.join(&*path);
-                if let Some(chunks) = chunks {
-                    chunks.fetch().await?.to_file(path).await?;
-                } else {
-                    async_fs::create_dir_all(path).await?;
-                }
-                Ok(())
-            })
-            .await?;
-        Ok(())
     }
 }
