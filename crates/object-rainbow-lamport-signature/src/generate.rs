ad556c79993751f61d1c155978c62dcd26b3567d
parrrate <alisa@parrrate.com>
2026-09-10 23:34:11 +0000

    `regenerate(rng)`

--- crates/object-rainbow-lamport-signature/src/generate.rs
+++ crates/object-rainbow-lamport-signature/src/generate.rs
@@ -1,41 +1,57 @@
-use rand::{TryRng, rngs::SysRng};
+use rand::{TryCryptoRng, rngs::SysRng};
 
 use crate::{PrivateFragment, PrivateKey, PrivatePair, SecretFragment};
 
 pub trait Generate: Default {
-    fn regenerate(&mut self) -> object_rainbow::Result<()>;
+    fn regenerate<R: ?Sized + TryCryptoRng>(&mut self, rng: &mut R) -> object_rainbow::Result<()>
+    where
+        std::io::Error: From<R::Error>;
     fn generate() -> object_rainbow::Result<Self> {
         let mut new = Self::default();
-        new.regenerate()?;
+        new.regenerate(&mut SysRng)?;
         Ok(new)
     }
 }
 
 impl Generate for SecretFragment {
-    fn regenerate(&mut self) -> object_rainbow::Result<()> {
-        SysRng
-            .try_fill_bytes(&mut self.0)
+    fn regenerate<R: ?Sized + TryCryptoRng>(&mut self, rng: &mut R) -> object_rainbow::Result<()>
+    where
+        std::io::Error: From<R::Error>,
+    {
+        rng.try_fill_bytes(&mut self.0)
             .map_err(std::io::Error::from)?;
         Ok(())
     }
 }
 
 impl Generate for PrivateFragment {
-    fn regenerate(&mut self) -> object_rainbow::Result<()> {
-        self.0.regenerate()
+    fn regenerate<R: ?Sized + TryCryptoRng>(&mut self, rng: &mut R) -> object_rainbow::Result<()>
+    where
+        std::io::Error: From<R::Error>,
+    {
+        self.0.regenerate(rng)
     }
 }
 
 impl Generate for PrivatePair {
-    fn regenerate(&mut self) -> object_rainbow::Result<()> {
+    fn regenerate<R: ?Sized + TryCryptoRng>(&mut self, rng: &mut R) -> object_rainbow::Result<()>
+    where
+        std::io::Error: From<R::Error>,
+    {
         self.0
             .iter_mut()
-            .try_for_each(|fragment| fragment.regenerate())
+            .try_for_each(|fragment| fragment.regenerate(rng))
     }
 }
 
 impl Generate for PrivateKey {
-    fn regenerate(&mut self) -> object_rainbow::Result<()> {
-        self.0.0.iter_mut().try_for_each(|pair| pair.regenerate())
+    fn regenerate<R: ?Sized + TryCryptoRng>(&mut self, rng: &mut R) -> object_rainbow::Result<()>
+    where
+        std::io::Error: From<R::Error>,
+    {
+        self.0
+            .0
+            .iter_mut()
+            .try_for_each(|pair| pair.regenerate(rng))
     }
 }
