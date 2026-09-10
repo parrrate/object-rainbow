daa76a909afcad01e8d0541ed659676636649294
parrrate <alisa@parrrate.com>
2026-09-10 20:32:47 +0000

    `From<Hash>` `into_bytes`

--- crates/object-rainbow/src/hash.rs
+++ crates/object-rainbow/src/hash.rs
@@ -85,6 +85,12 @@ impl Hash {
     }
 }
 
+impl From<Hash> for [u8; HASH_SIZE] {
+    fn from(hash: Hash) -> Self {
+        hash.into_bytes()
+    }
+}
+
 impl Deref for Hash {
     type Target = [u8; HASH_SIZE];
 
