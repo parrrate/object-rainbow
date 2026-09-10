d7b877a85fec9bc67441ab7b3df600c5df332599
parrrate <alisa@parrrate.com>
2026-09-10 22:58:34 +0000

    `#[pod] NoCollisions`

--- crates/object-rainbow-apply/src/enforce_unique.rs
+++ crates/object-rainbow-apply/src/enforce_unique.rs
@@ -1,6 +1,4 @@
-use object_rainbow::{
-    InlineOutput, ListHashes, Parse, ParseInline, Tagged, ToOutput, Topological, pod,
-};
+use object_rainbow::pod;
 
 use crate::Apply;
 
@@ -35,19 +33,7 @@ impl<D: Send, T: Apply<D, Output = X>, X: Collision<D, ExposedState, Output = O>
 
 /// This is separate from [`NoOverwrites`] both because of potential implementation conflicts and
 /// because of some semantical differences.
-#[derive(
-    ToOutput,
-    InlineOutput,
-    Tagged,
-    ListHashes,
-    Topological,
-    Parse,
-    ParseInline,
-    Clone,
-    PartialEq,
-    Eq,
-    Default,
-)]
+#[pod]
 pub struct NoCollisions<T>(T);
 
 impl<D: Send, T: Apply<D, Output = X>, X: Collision<D, ConcealedState, Output = O>, O: Send>
