d220c13d62d4fd615472f3443028398d0484bfb1
parrrate <alisa@parrrate.com>
2026-09-10 20:04:48 +0000

    `PublicFragment: PartialEq`

--- crates/object-rainbow-lamport-signature/src/lib.rs
+++ crates/object-rainbow-lamport-signature/src/lib.rs
@@ -15,7 +15,7 @@ pub struct PrivatePair([PrivateFragment; 2]);
 #[derive(Debug, ToOutput, Clone, Copy)]
 pub struct PrivateKey([PrivatePair; LENGTH]);
 
-#[derive(Debug, ToOutput, InlineOutput, Clone, Copy)]
+#[derive(Debug, ToOutput, InlineOutput, Clone, Copy, PartialEq)]
 pub struct PublicFragment(Hash);
 
 #[derive(Debug, ToOutput, InlineOutput, Clone, Copy)]
