7e789adbb95173e49f082cac5c47f1c261d8e91e
parrrate <alisa@parrrate.com>
2026-09-10 19:35:10 +0000

    move `LENGTH`

--- crates/object-rainbow-lamport-signature/src/lib.rs
+++ crates/object-rainbow-lamport-signature/src/lib.rs
@@ -1,12 +1,12 @@
 use object_rainbow::{HASH_SIZE, InlineOutput, ToOutput};
 
+const LENGTH: usize = HASH_SIZE * 8;
+
 #[derive(Debug, ToOutput, InlineOutput)]
 pub struct PrivateFragment([u8; HASH_SIZE]);
 
 #[derive(Debug, ToOutput, InlineOutput)]
 pub struct PrivatePair([PrivateFragment; 2]);
-
-const LENGTH: usize = HASH_SIZE * 8;
 
 #[derive(Debug, ToOutput)]
 pub struct PrivateKey([PrivatePair; LENGTH]);
