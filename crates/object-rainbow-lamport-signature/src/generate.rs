44d6d5e6b8de4f698cbe3bbbe92327f6c696d26d
parrrate <alisa@parrrate.com>
2026-09-10 23:26:53 +0000

    `SecretFragment::Regenerate`

--- crates/object-rainbow-lamport-signature/src/generate.rs
+++ crates/object-rainbow-lamport-signature/src/generate.rs
@@ -1,15 +1,19 @@
-use object_rainbow::HASH_SIZE;
 use rand::{TryRng, rngs::SysRng};
 
 use crate::{PrivateFragment, SecretFragment};
 
 impl SecretFragment {
-    pub fn generate() -> object_rainbow::Result<Self> {
-        let mut bytes = [0u8; HASH_SIZE];
+    pub fn regenerate(&mut self) -> object_rainbow::Result<()> {
         SysRng
-            .try_fill_bytes(&mut bytes)
+            .try_fill_bytes(&mut self.0)
             .map_err(std::io::Error::from)?;
-        Ok(Self(bytes))
+        Ok(())
+    }
+
+    pub fn generate() -> object_rainbow::Result<Self> {
+        let mut fragment = Self::default();
+        fragment.regenerate()?;
+        Ok(fragment)
     }
 }
 
