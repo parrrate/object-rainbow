231ac419ebf84b6f259b5f8b6c91faf4676c8616
parrrate <alisa@parrrate.com>
2026-09-10 20:02:20 +0000

    `Signature: Copy`

--- crates/object-rainbow-lamport-signature/src/lib.rs
+++ crates/object-rainbow-lamport-signature/src/lib.rs
@@ -27,7 +27,7 @@ pub struct PublicKey([PublicPair; LENGTH]);
 #[derive(Debug, ToOutput, InlineOutput, Clone, Copy)]
 pub struct SignatureFragment(SecretFragment);
 
-#[derive(Debug, ToOutput, Clone)]
+#[derive(Debug, ToOutput, Clone, Copy)]
 pub struct Signature([SignatureFragment; LENGTH]);
 
 pub struct MessageFragment(pub bool);
