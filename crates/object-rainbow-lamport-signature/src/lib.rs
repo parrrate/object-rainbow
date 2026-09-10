5d8d8ef8a48063b0d65151d6079a9786ce2a1e93
parrrate <alisa@parrrate.com>
2026-09-10 20:09:20 +0000

    `Message: Copy`

--- crates/object-rainbow-lamport-signature/src/lib.rs
+++ crates/object-rainbow-lamport-signature/src/lib.rs
@@ -33,7 +33,7 @@ pub struct Signature([SignatureFragment; LENGTH]);
 #[derive(Clone, Copy)]
 pub struct MessageFragment(pub bool);
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 pub struct Message(pub BitArray<[u8; HASH_SIZE]>);
 
 impl SecretFragment {
