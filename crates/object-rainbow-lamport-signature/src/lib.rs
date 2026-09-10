072556eb2982c7abc5ae7103d539f9343e2461eb
parrrate <alisa@parrrate.com>
2026-09-10 19:05:08 +0000

    `PrivatePair`

--- crates/object-rainbow-lamport-signature/src/lib.rs
+++ crates/object-rainbow-lamport-signature/src/lib.rs
@@ -2,3 +2,6 @@ use object_rainbow::{HASH_SIZE, InlineOutput, ToOutput};
 
 #[derive(Debug, ToOutput, InlineOutput)]
 pub struct PrivateFragment([u8; HASH_SIZE]);
+
+#[derive(Debug, ToOutput)]
+pub struct PrivatePair([PrivateFragment; 2]);
