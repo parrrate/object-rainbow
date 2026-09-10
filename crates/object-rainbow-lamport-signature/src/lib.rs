eec0cf531332ad54a284fb647dcd12a2debf7fa9
parrrate <alisa@parrrate.com>
2026-09-10 20:03:13 +0000

    `PublicPair: Copy`

--- crates/object-rainbow-lamport-signature/src/lib.rs
+++ crates/object-rainbow-lamport-signature/src/lib.rs
@@ -18,7 +18,7 @@ pub struct PrivateKey([PrivatePair; LENGTH]);
 #[derive(Debug, ToOutput, InlineOutput, Clone, Copy)]
 pub struct PublicFragment(Hash);
 
-#[derive(Debug, ToOutput, InlineOutput, Clone)]
+#[derive(Debug, ToOutput, InlineOutput, Clone, Copy)]
 pub struct PublicPair([PublicFragment; 2]);
 
 #[derive(Debug, ToOutput)]
