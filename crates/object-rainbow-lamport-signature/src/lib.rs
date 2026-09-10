ce9b8029477c7cbce4a4fcad9ed0681ab4c10486
parrrate <alisa@parrrate.com>
2026-09-10 21:38:46 +0000

    `FragmentSequence: ListHashes`

--- crates/object-rainbow-lamport-signature/src/lib.rs
+++ crates/object-rainbow-lamport-signature/src/lib.rs
@@ -1,11 +1,11 @@
 use std::ops::Index;
 
 use bitvec::array::BitArray;
-use object_rainbow::{HASH_SIZE, Hash, InlineOutput, Tagged, ToOutput, pod};
+use object_rainbow::{HASH_SIZE, Hash, InlineOutput, ListHashes, Tagged, ToOutput, pod};
 
 const LENGTH: usize = HASH_SIZE * 8;
 
-#[derive(Debug, ToOutput, InlineOutput, Tagged, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, ToOutput, InlineOutput, Tagged, ListHashes, Clone, Copy, PartialEq, Eq)]
 pub struct FragmentSequence<T>([T; LENGTH]);
 
 impl<T: Default> Default for FragmentSequence<T> {
