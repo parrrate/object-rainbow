8bfd3a214a9066ecbcdbfb6cf120cae793f3d360
parrrate <alisa@parrrate.com>
2026-09-11 01:04:51 +0000

    `Addressed: ToOutput`

--- crates/object-rainbow/src/addressed.rs
+++ crates/object-rainbow/src/addressed.rs
@@ -63,7 +63,7 @@ impl<I: PointInput> ParseInline<I> for Arc<dyn Singular> {
     }
 }
 
-#[derive(Parse, ParseInline)]
+#[derive(ToOutput, Parse, ParseInline)]
 pub struct Addressed<T, Extra> {
     inner: AddressedBytes,
     extra: Extras<Extra>,
