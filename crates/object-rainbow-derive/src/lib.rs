a78e0d6f0f498feec7ccb65dd5e9f37c3eb1f3fa
parrrate <alisa@parrrate.com>
2026-09-11 13:52:32 +0000

    `#[derive(CanonicalExtra)]`

--- crates/object-rainbow-derive/src/lib.rs
+++ crates/object-rainbow-derive/src/lib.rs
@@ -4,6 +4,7 @@ use std::collections::BTreeSet;
 
 use darling::{FromMeta, util::SpannedValue};
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::{ToTokens, quote, quote_spanned};
 use syn::{
     Attribute, Data, DeriveInput, Error, Expr, FnArg, GenericParam, Generics, Ident, ImplItem,
@@ -2714,6 +2715,130 @@ fn gen_mn_array(data: &Data, attrs: &[Attribute]) -> proc_macro2::TokenStream {
         }
         Data::Union(data) => {
             Error::new_spanned(data.union_token, "`union`s are not supported").into_compile_error()
+        }
+    }
+}
+
+#[proc_macro_derive(CanonicalExtra)]
+pub fn derive_canonical_extra(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let generics = input.generics.clone();
+    let (_, ty_generics, _) = generics.split_for_impl();
+    let generics = match bounds_canonical_extra(input.generics, &input.data) {
+        Ok(g) => g,
+        Err(e) => return e.into_compile_error().into(),
+    };
+    let canonical_extra = gen_canonical_extra(&input.data);
+    let (impl_generics, _, where_clause) = generics.split_for_impl();
+    let target = parse_for(&name, &input.attrs);
+    let output = quote! {
+        #[automatically_derived]
+        impl #impl_generics ::object_rainbow::CanonicalExtra for #target #ty_generics #where_clause {
+            type Extra = __Extra;
+
+            fn canonical_extra(&self) -> Self::Extra {
+                #canonical_extra
+            }
+        }
+    };
+    TokenStream::from(output)
+}
+
+fn bounds_canonical_extra(mut generics: Generics, data: &Data) -> syn::Result<Generics> {
+    match data {
+        Data::Struct(data) => {
+            if let Some(f) = data.fields.iter().next() {
+                let ty = &f.ty;
+                generics
+                    .make_where_clause()
+                    .predicates
+                    .push(parse_quote_spanned! { ty.span() =>
+                        #ty: ::object_rainbow::CanonicalExtra<Extra = __Extra>
+                    });
+            }
+        }
+        Data::Enum(data) => {
+            for variant in &data.variants {
+                if let Some(f) = variant.fields.iter().next() {
+                    let ty = &f.ty;
+                    generics.make_where_clause().predicates.push(
+                        parse_quote_spanned! { ty.span() =>
+                            #ty: ::object_rainbow::CanonicalExtra<Extra = __Extra>
+                        },
+                    );
+                }
+            }
+        }
+        Data::Union(data) => {
+            return Err(Error::new_spanned(
+                data.union_token,
+                "`union`s are not supported",
+            ));
+        }
+    }
+    generics.params.push(parse_quote!(__Extra));
+    Ok(generics)
+}
+
+fn fields_canonicai_extra(
+    fields: &syn::Fields,
+    unit_span: Option<Span>,
+) -> proc_macro2::TokenStream {
+    match fields {
+        syn::Fields::Named(fields) => {
+            if let Some(f) = fields.named.iter().next() {
+                let ident = f.ident.as_ref().unwrap();
+                quote! {
+                    { #ident, .. }
+                        => ::object_rainbow::CanonicalExtra::canonical_extra(#ident),
+                }
+            } else {
+                let span = fields.brace_token.span.close();
+                syn::Error::new(span, "no fields").into_compile_error()
+            }
+        }
+        syn::Fields::Unnamed(fields) => {
+            if !fields.unnamed.is_empty() {
+                quote! {
+                    (__first, ..)
+                        => ::object_rainbow::CanonicalExtra::canonical_extra(__first),
+                }
+            } else {
+                let span = fields.paren_token.span.close();
+                syn::Error::new(span, "no fields").into_compile_error()
+            }
+        }
+        syn::Fields::Unit => syn::Error::new(unit_span.unwrap(), "no fields").into_compile_error(),
+    }
+}
+
+fn gen_canonical_extra(data: &Data) -> proc_macro2::TokenStream {
+    match data {
+        Data::Struct(data) => {
+            let arm = fields_canonicai_extra(&data.fields, data.semi_token.map(|x| x.span));
+            quote! {
+                match self {
+                    Self #arm
+                }
+            }
+        }
+        Data::Enum(data) => {
+            let arms = data.variants.iter().map(|variant| {
+                let ident = &variant.ident;
+                let arm = fields_canonicai_extra(&variant.fields, Some(ident.span()));
+                quote! {
+                    Self::#ident #arm
+                }
+            });
+            quote! {
+                match self {
+                    #(#arms)*
+                }
+            }
+        }
+        Data::Union(data) => {
+            Error::new_spanned(data.union_token, "`union`s are not supported").to_compile_error()
         }
     }
 }
