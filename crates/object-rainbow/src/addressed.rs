95350bd6e6ef8b432a410788f69eb1e757e4facd
parrrate <alisa@parrrate.com>
2026-09-11 01:05:29 +0000

    `Addressed: ListHashes`

--- crates/object-rainbow/src/addressed.rs
+++ crates/object-rainbow/src/addressed.rs
@@ -63,7 +63,7 @@ impl<I: PointInput> ParseInline<I> for Arc<dyn Singular> {
     }
 }
 
-#[derive(ToOutput, InlineOutput, Tagged, Parse, ParseInline)]
+#[derive(ToOutput, InlineOutput, Tagged, ListHashes, Parse, ParseInline)]
 pub struct Addressed<T, Extra> {
     inner: AddressedBytes,
     extra: Extras<Extra>,
