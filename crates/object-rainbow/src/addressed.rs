44c65da39c823b3c3b23e3e05b8e211d7b0236f3
parrrate <alisa@parrrate.com>
2026-09-11 00:56:00 +0000

    `AddressedBytes: InlineOutput`

--- crates/object-rainbow/src/addressed.rs
+++ crates/object-rainbow/src/addressed.rs
@@ -13,7 +13,7 @@ pub trait ExtractResolve: FetchBytes {
 
 impl<T: ?Sized + FetchBytes> ExtractResolve for T {}
 
-#[derive(Clone, ToOutput, Parse, ParseInline)]
+#[derive(Clone, ToOutput, InlineOutput, Parse, ParseInline)]
 pub struct AddressedBytes {
     pub address: Address,
     pub resolve: Arc<dyn Resolve>,
