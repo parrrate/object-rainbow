11f9bbfcc52f1f2c37553ed179de4f76fef0f9aa
parrrate <alisa@parrrate.com>
2026-09-11 12:36:20 +0000

    `dyn Fetch: InlineOutput`

--- crates/object-rainbow/src/lib.rs
+++ crates/object-rainbow/src/lib.rs
@@ -319,6 +319,8 @@ impl<T> ToOutput for dyn Fetch<T = T> {
     fn to_output(&self, _: &mut impl Output) {}
 }
 
+impl<T> InlineOutput for dyn Fetch<T = T> {}
+
 impl<T: Tagged> Tagged for dyn Fetch<T = T> {
     const TAGS: Tags = T::TAGS;
     const HASH: Hash = T::HASH;
