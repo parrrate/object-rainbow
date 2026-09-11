f4be104c7cc389a9935a51341158b1688c007fd4
parrrate <alisa@parrrate.com>
2026-09-11 01:03:46 +0000

    `AddressedBytes: MaybeHasNiche`

--- crates/object-rainbow/src/addressed.rs
+++ crates/object-rainbow/src/addressed.rs
@@ -13,7 +13,9 @@ pub trait ExtractResolve: FetchBytes {
 
 impl<T: ?Sized + FetchBytes> ExtractResolve for T {}
 
-#[derive(Clone, ToOutput, InlineOutput, Tagged, ListHashes, Parse, ParseInline, Size)]
+#[derive(
+    Clone, ToOutput, InlineOutput, Tagged, ListHashes, Parse, ParseInline, Size, MaybeHasNiche,
+)]
 pub struct AddressedBytes {
     pub address: Address,
     pub resolve: Arc<dyn Resolve>,
