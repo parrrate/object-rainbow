aaa0964a71e1bdb1fa6969f886a3e72ac51c4035
parrrate <alisa@parrrate.com>
2026-09-11 00:31:11 +0000

    move `Addressed`

--- crates/object-rainbow-point/src/lib.rs
+++ crates/object-rainbow-point/src/lib.rs
@@ -16,7 +16,7 @@ use object_rainbow::{
     FetchBytes, FullHash, Hash, InlineOutput, ListHashes, MaybeHasNiche, Node, OptionalHash,
     Output, Parse, ParseAsInline, ParseInline, PointInput, PointVisitor, Resolve, Singular,
     SingularFetch, Size, Tagged, ToOutput, Topological, Traversible,
-    addressed::AddressedBytes,
+    addressed::{Addressed, AddressedBytes},
     extras::fetch_extra::{ParseFetch, ParseFetchInline},
     object_marker::ObjectMarker,
 };
@@ -25,77 +25,6 @@ use object_rainbow::{
 mod point_deserialize;
 #[cfg(feature = "point-serialize")]
 mod point_serialize;
-
-struct Addressed<T, Extra> {
-    inner: AddressedBytes,
-    extra: Extras<Extra>,
-    _object: PhantomData<fn() -> T>,
-}
-
-impl<T, Extra> Addressed<T, Extra> {
-    fn from_inner(inner: AddressedBytes, extra: Extra) -> Self {
-        Self {
-            inner,
-            extra: Extras(extra),
-            _object: PhantomData,
-        }
-    }
-}
-
-impl<T, Extra> FetchBytes for Addressed<T, Extra> {
-    fn fetch_bytes(&'_ self) -> FailFuture<'_, ByteNode> {
-        self.inner.fetch_bytes()
-    }
-
-    fn fetch_data(&'_ self) -> FailFuture<'_, Vec<u8>> {
-        self.inner.fetch_data()
-    }
-
-    fn fetch_bytes_local(&self) -> object_rainbow::Result<Option<ByteNode>> {
-        self.inner.fetch_bytes_local()
-    }
-
-    fn as_inner(&self) -> Option<&dyn Any> {
-        Some(&self.inner)
-    }
-
-    fn as_resolve(&self) -> Option<&Arc<dyn Resolve>> {
-        self.inner.as_resolve()
-    }
-
-    fn try_unwrap_resolve(self: Arc<Self>) -> Option<Arc<dyn Resolve>> {
-        Arc::try_unwrap(self)
-            .ok()
-            .map(|Self { inner, .. }| inner.resolve)
-    }
-}
-
-impl<T, Extra: Send + Sync> Singular for Addressed<T, Extra> {
-    fn hash(&self) -> Hash {
-        self.inner.hash()
-    }
-}
-
-impl<T: FullHash, Extra: Send + Sync + ExtraFor<T>> Fetch for Addressed<T, Extra> {
-    type T = T;
-
-    fn fetch(&'_ self) -> FailFuture<'_, Self::T> {
-        Box::pin(async {
-            let (data, resolve) = self.fetch_bytes().await?;
-            self.extra.parse_checked(self.inner.hash(), &data, &resolve)
-        })
-    }
-
-    fn try_fetch_local(&self) -> object_rainbow::Result<Option<Node<Self::T>>> {
-        let Some((data, resolve)) = self.fetch_bytes_local()? else {
-            return Ok(None);
-        };
-        let object = self
-            .extra
-            .parse_checked(self.inner.hash(), &data, &resolve)?;
-        Ok(Some((object, resolve)))
-    }
-}
 
 struct FetchExtra<T, D> {
     inner: AddressedBytes,
--- crates/object-rainbow/src/addressed.rs
+++ crates/object-rainbow/src/addressed.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use crate::*;
+use crate::{extras::Extras, *};
 
 pub trait ExtractResolve: FetchBytes {
     fn extract_resolve<R: Any>(&self) -> Option<(&Address, &R)> {
@@ -58,5 +58,76 @@ impl<I: PointInput> Parse<I> for Arc<dyn Singular> {
 impl<I: PointInput> ParseInline<I> for Arc<dyn Singular> {
     fn parse_inline(input: &mut I) -> crate::Result<Self> {
         Ok(Arc::new(input.parse_inline::<AddressedBytes>()?))
+    }
+}
+
+pub struct Addressed<T, Extra> {
+    inner: AddressedBytes,
+    extra: Extras<Extra>,
+    _object: PhantomData<fn() -> T>,
+}
+
+impl<T, Extra> Addressed<T, Extra> {
+    pub fn from_inner(inner: AddressedBytes, extra: Extra) -> Self {
+        Self {
+            inner,
+            extra: Extras(extra),
+            _object: PhantomData,
+        }
+    }
+}
+
+impl<T, Extra> FetchBytes for Addressed<T, Extra> {
+    fn fetch_bytes(&'_ self) -> FailFuture<'_, ByteNode> {
+        self.inner.fetch_bytes()
+    }
+
+    fn fetch_data(&'_ self) -> FailFuture<'_, Vec<u8>> {
+        self.inner.fetch_data()
+    }
+
+    fn fetch_bytes_local(&self) -> object_rainbow::Result<Option<ByteNode>> {
+        self.inner.fetch_bytes_local()
+    }
+
+    fn as_inner(&self) -> Option<&dyn Any> {
+        Some(&self.inner)
+    }
+
+    fn as_resolve(&self) -> Option<&Arc<dyn Resolve>> {
+        self.inner.as_resolve()
+    }
+
+    fn try_unwrap_resolve(self: Arc<Self>) -> Option<Arc<dyn Resolve>> {
+        Arc::try_unwrap(self)
+            .ok()
+            .map(|Self { inner, .. }| inner.resolve)
+    }
+}
+
+impl<T, Extra: Send + Sync> Singular for Addressed<T, Extra> {
+    fn hash(&self) -> Hash {
+        self.inner.hash()
+    }
+}
+
+impl<T: FullHash, Extra: Send + Sync + ExtraFor<T>> Fetch for Addressed<T, Extra> {
+    type T = T;
+
+    fn fetch(&'_ self) -> FailFuture<'_, Self::T> {
+        Box::pin(async {
+            let (data, resolve) = self.fetch_bytes().await?;
+            self.extra.parse_checked(self.inner.hash(), &data, &resolve)
+        })
+    }
+
+    fn try_fetch_local(&self) -> object_rainbow::Result<Option<Node<Self::T>>> {
+        let Some((data, resolve)) = self.fetch_bytes_local()? else {
+            return Ok(None);
+        };
+        let object = self
+            .extra
+            .parse_checked(self.inner.hash(), &data, &resolve)?;
+        Ok(Some((object, resolve)))
     }
 }
