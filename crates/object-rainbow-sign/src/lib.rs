5fd36e404960481501d7dd5dcecf54a81fce1aa7
parrrate <alisa@parrrate.com>
2026-09-12 22:03:12 +0000

    `VerifyKey`

--- crates/object-rainbow-sign/src/lib.rs
+++ crates/object-rainbow-sign/src/lib.rs
@@ -1,1 +1,6 @@
+use object_rainbow::Hash;
 
+pub trait VerifyKey<Signature, Message = Hash> {
+    type Error: 'static + Send + Sync + std::error::Error;
+    fn verify(&self, signature: &Signature, message: &Message) -> Result<(), Self::Error>;
+}
