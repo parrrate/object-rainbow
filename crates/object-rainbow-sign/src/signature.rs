ec31418ce36a059d3b9de4cf1309b83438d7f005
parrrate <alisa@parrrate.com>
2026-09-12 23:15:50 +0000

    `Signer` blanket

--- crates/object-rainbow-sign/src/signature.rs
+++ crates/object-rainbow-sign/src/signature.rs
@@ -1,11 +1,27 @@
-use signature::Verifier;
+use signature::{Signer, Verifier};
 
-use crate::VerifyKey;
+use crate::{SigningKey, VerifyKey};
 
 impl<V: Send + Sync + Eq + Verifier<S>, S, M: AsRef<[u8]>> VerifyKey<S, M> for V {
     type Error = signature::Error;
 
     fn verify(&self, signature: &S, message: &M) -> Result<(), Self::Error> {
         self.verify(message.as_ref(), signature)
+    }
+}
+
+impl<
+    K: AsRef<V> + Signer<S>,
+    V: Send + Sync + Clone + Eq + Verifier<S>,
+    S: Send + Sync,
+    M: AsRef<[u8]>,
+> SigningKey<V, S, M> for K
+{
+    fn sign(&self, message: &M) -> S {
+        self.sign(message.as_ref())
+    }
+
+    fn to_verify_key(&self) -> V {
+        self.as_ref().clone()
     }
 }
