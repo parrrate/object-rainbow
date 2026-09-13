d397a0c0bd5b5b29f05c0f27ffadd5bcb3edca42
parrrate <alisa@parrrate.com>
2026-09-13 22:49:42 +0000

    `ToOutputDyn` blanket

--- crates/object-rainbow-opaque/src/lib.rs
+++ crates/object-rainbow-opaque/src/lib.rs
@@ -1,5 +1,11 @@
-use object_rainbow::Output;
+use object_rainbow::{Output, ToOutput};
 
 pub trait ToOutputDyn {
     fn to_output_dyn(&self, output: &mut dyn Output);
+}
+
+impl<T: ?Sized + ToOutput> ToOutputDyn for T {
+    fn to_output_dyn(&self, output: &mut dyn Output) {
+        self.to_output(output);
+    }
 }
