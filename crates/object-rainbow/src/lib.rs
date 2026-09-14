a951d4320b4cd26f804c8ce0e2b85f0604a4f9aa
parrrate <alisa@parrrate.com>
2026-09-14 18:34:45 +0000

    `FromOutput::Output`

--- crates/object-rainbow/src/lib.rs
+++ crates/object-rainbow/src/lib.rs
@@ -1338,7 +1338,9 @@ pub trait ReflessInline:
 
 impl<T: ReflessObject + InlineOutput + for<'a> ParseInline<ReflessInput<'a>>> ReflessInline for T {}
 
-pub trait FromOutput {}
+pub trait FromOutput {
+    type Output;
+}
 
 pub trait Output {
     fn write(&mut self, data: &[u8]);
