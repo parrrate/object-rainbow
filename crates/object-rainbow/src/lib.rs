ac2ec2a331ecbf727b8edf3b0ba6afd0af446277
parrrate <alisa@parrrate.com>
2026-09-14 19:32:45 +0000

    `AsExtra` blanket

--- crates/object-rainbow/src/lib.rs
+++ crates/object-rainbow/src/lib.rs
@@ -2025,3 +2025,5 @@ impl<A: CanonicalExtra, B> CanonicalExtra for (A, B) {
 }
 
 pub trait AsExtra: CanonicalExtra + AsRef<Self::Extra> {}
+
+impl<T: ?Sized + CanonicalExtra + AsRef<T::Extra>> AsExtra for T {}
