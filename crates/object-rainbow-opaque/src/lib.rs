d6d57e75a07c10b1fb5645f364a7e7bca6a71791
parrrate <alisa@parrrate.com>
2026-09-14 00:12:40 +0000

    `OpaqueFactory: Clone`

--- crates/object-rainbow-opaque/src/lib.rs
+++ crates/object-rainbow-opaque/src/lib.rs
@@ -125,6 +125,7 @@ impl<I: PointInput<Extra = Arc<dyn Send + Sync + ExtraFor<ParsedOpaque>>>> Parse
     }
 }
 
+#[derive(Clone)]
 pub struct OpaqueFactory<Extra, T> {
     pub extra: Extra,
     pub object: ObjectMarker<T>,
