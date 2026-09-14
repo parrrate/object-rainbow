889db628db34016f6cbf255d3836e1eb8c62bda6
parrrate <alisa@parrrate.com>
2026-09-14 00:03:44 +0000

    `Opaque: ListHashes`

--- crates/object-rainbow-opaque/src/lib.rs
+++ crates/object-rainbow-opaque/src/lib.rs
@@ -111,7 +111,7 @@ impl<T: Traversible> TopologicalDyn for T {
     }
 }
 
-#[derive(ToOutput, Tagged)]
+#[derive(ToOutput, Tagged, ListHashes)]
 pub struct Opaque(pub Arc<dyn TraversibleDyn>);
 
 impl<I: PointInput<Extra = Arc<dyn Send + Sync + ExtraFor<Arc<dyn TraversibleDyn>>>>> Parse<I>
