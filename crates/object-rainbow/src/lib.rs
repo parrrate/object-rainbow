f76462794c58c2c9e15e9f5f30d96deb689603fe
parrrate <alisa@parrrate.com>
2026-09-15 23:40:29 +0000

    `'_ + Resolve: ListHashes`

--- crates/object-rainbow/src/lib.rs
+++ crates/object-rainbow/src/lib.rs
@@ -244,7 +244,7 @@ impl ToOutput for dyn '_ + Resolve {
 
 impl InlineOutput for dyn '_ + Resolve {}
 impl Tagged for dyn '_ + Resolve {}
-impl ListHashes for dyn Resolve {}
+impl ListHashes for dyn '_ + Resolve {}
 
 impl Size for dyn Resolve {
     type Size = typenum::U0;
